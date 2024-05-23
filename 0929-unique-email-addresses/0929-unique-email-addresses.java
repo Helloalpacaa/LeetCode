14f86f3d106f06d1d87f4d9e989845917b2ead10
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-05-23 07:19:30 -0400

    Time: 16 ms (49.16%), Space: 45.2 MB (33.00%) - LeetHub

--- 0929-unique-email-addresses/0929-unique-email-addresses.java
+++ 0929-unique-email-addresses/0929-unique-email-addresses.java
@@ -2,20 +2,23 @@ class Solution {
     public int numUniqueEmails(String[] emails) {
         Set<String> uniqueEmails = new HashSet<>();
         for (String email: emails) {
-            String formattedEmail = getFormattedEmail(email);
+            String formattedEmail = formattedEmail(email);
             uniqueEmails.add(formattedEmail);
         }
         return uniqueEmails.size();
     }
     
-    private String getFormattedEmail (String email) {
+    private String formattedEmail(String email) {
+        // Split into localName and domainName
         String[] arr = email.split("@");
         String localName = arr[0];
         String domainName = arr[1];
         
+        // Split localName if there is "+"
         String[] localNameWithPlus = localName.split("\\+");
         localName = localNameWithPlus[0];
         
+        // Replace "." to ""
         localName = localName.replace(".", "");
         
         return localName + "@" + domainName;
