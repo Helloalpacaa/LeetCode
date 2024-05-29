0f9e4edcb797896e23fada75db417e5e242ba9f7
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-05-29 12:38:24 -0400

    Time: 65 ms (29.78%), Space: 52.5 MB (12.18%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -1,12 +1,10 @@
 class Solution {
     public int sumSubarrayMins(int[] arr) {
         int n = arr.length;
-        int[] pse = new int[n]; // previous smaller element, default value is -1
-        int[] nse = new int[n]; // next smaller element, default value is n
-        
+        int[] pse = new int[n];
+        int[] nse = new int[n];
         Stack<Integer> stack = new Stack<>();
         
-        // 栈里只存一个值或者不存值，当遇到1时，1比3小，pop 3，这时栈为空，说明1左边没有比它小的值，单调递增栈
         for (int i = 0; i < n; i++) {
             while (!stack.isEmpty() && arr[i] < arr[stack.peek()]) {
                 stack.pop();
@@ -25,12 +23,12 @@ class Solution {
             stack.push(i);
         }
         
-        long sumSubarrayMins = 0;
         int mod = 1000000007;
+        long sumSubarrayMins = 0;  
         
         for (int i = 0; i < n; i++) {
-            long contribution = (long) arr[i] * (i - pse[i]) * (nse[i] - i);
-            sumSubarrayMins = (sumSubarrayMins + contribution) % mod;
+            long sum = (long) arr[i] * (i - pse[i]) * (nse[i] - i) ;
+            sumSubarrayMins = (sumSubarrayMins + sum) % mod;
         }
         
         return (int) sumSubarrayMins;
