b18407439f50685c53986edc54ac31558b9a2220
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-05-29 12:50:00 -0400

    Time: 6 ms (94.08%), Space: 44.4 MB (95.73%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.java
+++ 0402-remove-k-digits/0402-remove-k-digits.java
@@ -4,23 +4,22 @@ class Solution {
             return "0";
         }
         
-        // 用stringBuilder模拟stack
         StringBuilder sb = new StringBuilder();
+        int n = num.length();
+        
         for (char c: num.toCharArray()) {
-            while (k > 0 && sb.length() > 0 && c < sb.charAt(sb.length() - 1)) {
+            while (sb.length() != 0 && k > 0 && c < sb.charAt(sb.length() - 1)) {
                 sb.deleteCharAt(sb.length() - 1);
                 k--;
             }
             sb.append(c);
         }
         
-        // 看是否要去除掉最后一位
         while (k > 0) {
             sb.deleteCharAt(sb.length() - 1);
             k--;
         }
         
-        // 去除example 2里的首字母为‘0’的情况
         while (sb.length() > 1 && sb.charAt(0) == '0') {
             sb.deleteCharAt(0);
         }
