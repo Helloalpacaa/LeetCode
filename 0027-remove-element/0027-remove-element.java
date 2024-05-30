cb65d8e5a0cf7134d27813d22aff43b784dab29a
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-05-30 09:09:53 -0400

    Time: 0 ms (100.00%), Space: 41.8 MB (37.47%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,21 +1,12 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int i = 0;
-        int j = nums.length - 1;
-        while (i <= j) {
-            if (nums[i] == val) {
-                swap(nums, i, j);
-                j--;
-            } else {
-                i++;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[count++] = nums[i];
             }
         }
-        return i;
-    }
-    
-    private void swap(int[] nums, int i, int j) {
-        int tmp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = tmp;
+        
+        return count;
     }
 }
