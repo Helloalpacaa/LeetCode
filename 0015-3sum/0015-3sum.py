51e8b25234f80481c8b9478572aa8778a454e1ea
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-06-09 08:08:17 -0400

    Time: 546 ms (90.79%), Space: 20.5 MB (78.35%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,17 +1,18 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        ans = []
         
-        for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-                
+        ans = []
+        for i in range(len(nums) - 2):
             if nums[i] > 0:
                 break
-                
+            
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            
             j = i + 1
             k = len(nums) - 1
+            
             while j < k:
                 threeSum = nums[i] + nums[j] + nums[k]
                 if threeSum < 0:
@@ -20,11 +21,11 @@ class Solution:
                     k -= 1
                 else:
                     ans.append([nums[i], nums[j], nums[k]])
-                    j += 1
-                    while j < k and nums[j] == nums[j - 1]:
+                    while j < k and nums[j + 1] == nums[j]:
                         j += 1
-                    k -= 1
-                    while j < k and nums[k] == nums[k + 1]:
+                    while j < k and nums[k - 1] == nums[k]:
                         k -= 1
-            
+                    j += 1
+                    k -= 1
+        
         return ans
