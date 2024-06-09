95070b708472b81ea8d5594ade8c2a34b1008b19
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-06-09 08:27:50 -0400

    Time: 42 ms (72.57%), Space: 17 MB (86.30%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -2,6 +2,7 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         i = 0
         j = len(s) - 1
+        
         while i < j:
             while i < j and not s[i].isalnum():
                 i += 1
@@ -13,4 +14,3 @@ class Solution:
             j -= 1
         
         return True
-            
