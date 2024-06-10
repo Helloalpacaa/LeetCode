9b320dbbe8a6f9e41e380b3791fa5ce83bc0b035
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-06-09 21:02:43 -0400

    Time: 155 ms (71.75%), Space: 27.9 MB (60.15%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,13 +3,15 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        def swipe(nums: List[int], i: int, j: int) -> None:
-            while i < j:
-                nums[i], nums[j] = nums[j], nums[i]
-                i += 1
-                j -= 1
-        
         k = k % len(nums)
-        swipe(nums, 0, len(nums) - 1)
-        swipe(nums, 0, k - 1)
-        swipe(nums, k, len(nums) - 1)
+        self.reverse(nums, 0, len(nums) - 1)
+        self.reverse(nums, 0, k - 1)
+        self.reverse(nums, k, len(nums) - 1)
+    
+    
+    def reverse(self, nums: List[int], i: int, j: int) -> None:
+        while i < j:
+            nums[i], nums[j] = nums[j], nums[i]
+            i += 1
+            j -= 1
+        
