4cb817dcfda1ad3de5d5972ec0751f1c8098086a
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-06-09 20:51:36 -0400

    Time: 35 ms (89.52%), Space: 16.5 MB (82.56%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -1,16 +1,17 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        edge = {c: i for i, c in enumerate(s)} # using enumerate() to get both the index i and the character c, store the index as value and the character as the key in the dictionary
+        edge = {}
+        for index, value in enumerate(s):
+            edge[value] = index
         
         ans = []
         left = 0
         right = 0
-        for i, c in enumerate(s):
-            right = max(right, edge[c])
+        for i in range(len(s)):
+            right = max(right, edge[s[i]])
             if i == right:
                 ans.append(right - left + 1)
                 left = right + 1
         
         return ans
-        
-        
+            
