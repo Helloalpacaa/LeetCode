28b452e9d9b6d31f02ec4985b2fd1748390797e7
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-06-11 21:26:32 -0400

    Time: 287 ms (65.84%), Space: 32.2 MB (62.90%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -20,7 +20,7 @@ class Solution:
         
         slow = head
         fast = prev
-        while slow and fast and slow != fast:
+        while fast:
             if slow.val != fast.val:
                 return False
             slow = slow.next
