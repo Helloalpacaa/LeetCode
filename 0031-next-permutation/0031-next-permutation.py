1afc645ca578a778c120a4b94a37cee7e1490f53
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-06-12 09:05:16 -0400

    Time: 44 ms (49.23%), Space: 16.5 MB (83.91%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -4,14 +4,14 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         i = len(nums) - 2
+        
         while i >= 0 and nums[i] >= nums[i + 1]:
-             i -= 1
+            i -= 1
         
         if i >= 0:
             j = len(nums) - 1
             while j >= 0 and nums[j] <= nums[i]:
                 j -= 1
-            
             nums[i], nums[j] = nums[j], nums[i]
         
         left = i + 1
@@ -22,4 +22,4 @@ class Solution:
             right -= 1
         
         
-        
+    
