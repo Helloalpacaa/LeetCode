769c53f6e39d0c7d3a7911f0cd3535c08aabb009
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-06-14 21:31:20 -0400

    Time: 389 ms (92.53%), Space: 26.4 MB (62.92%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,13 +1,18 @@
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
-        sortedList = []
+        sortedList = [] # sorted list to continuesly adding nums and sort them
         i = 0
         
         for j in range(len(nums)):
+            # use binary search to insert num
             bisect.insort(sortedList, nums[j])
             
+            # when max - min >limit
             if sortedList[-1] - sortedList[0] > limit:
+                # move i, so we need to pop nums[i] from sortedList
+                # use bisect.bisect to find the index of nums[i]
                 sortedList.pop(bisect.bisect(sortedList, nums[i]) - 1)
+                # move i one step, because we moved j, if the condition is not meet and we move i, the length of longest subarray won't change
                 i += 1
         
         return len(nums) - i
