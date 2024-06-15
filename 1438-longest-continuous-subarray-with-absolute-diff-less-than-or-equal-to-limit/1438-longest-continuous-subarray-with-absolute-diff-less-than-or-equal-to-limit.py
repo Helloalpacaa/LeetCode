5c77cf1c977d4b51a2b4ca86a6b97b4e4728d118
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-06-15 07:17:22 -0400

    Time: 633 ms (22.83%), Space: 36.1 MB (10.76%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,19 +1,26 @@
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
-        sortedList = [] # sorted list to continuesly adding nums and sort them
+        minHeap = [] # store the tuple(value, index) into a minHeap
+        maxHeap = [] # store the tuple(value, index) into a maxHeap  
+
         i = 0
-        
+        ans = 0
         for j in range(len(nums)):
-            # use binary search to insert num
-            bisect.insort(sortedList, nums[j])
+            heapq.heappush(minHeap, (nums[j], j))
+            heapq.heappush(maxHeap, (-nums[j], j))
             
-            # when max - min >limit
-            if sortedList[-1] - sortedList[0] > limit:
-                # move i, so we need to pop nums[i] from sortedList
-                # use bisect.bisect to find the index of nums[i]
-                sortedList.pop(bisect.bisect(sortedList, nums[i]) - 1)
-                # move i one step, because we moved j, if the condition is not meet and we move i, the length of longest subarray won't change
-                i += 1
+            # compare max - min with limit
+            while -maxHeap[0][0] - minHeap[0][0] > limit:
+                # move i to the new start of a valid sliding window
+                i = min(minHeap[0][1], maxHeap[0][1]) + 1
+                
+                # pop all the elements before nums[i]
+                while minHeap[0][1] < i:
+                    heapq.heappop(minHeap)
+                while maxHeap[0][1] < i:
+                    heapq.heappop(maxHeap)
+            
+            ans = max(ans, j - i + 1)
         
-        return len(nums) - i
+        return ans
         
