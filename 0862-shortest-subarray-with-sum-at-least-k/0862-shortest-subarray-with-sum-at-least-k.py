7d7820747cb48f53a0efc3afc293f6a0ffbd5f38
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-06-15 11:45:54 -0400

    Time: 888 ms (59.30%), Space: 30.7 MB (93.55%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
@@ -3,18 +3,18 @@ class Solution:
         n = len(nums)
         prefixSum = [0] * (n + 1)
         for i in range(1, n + 1):
-            prefixSum[i] = prefixSum[i - 1] + nums[i - 1]
-
+            prefixSum[i] = nums[i - 1] + prefixSum[i - 1]
+        
         ans = float('inf')
         queue = deque()
-
-        for i in range(n + 1):
-            while queue and prefixSum[i] - prefixSum[queue[0]] >= k:
-                ans = min(ans, i - queue.popleft())
-
-            while queue and prefixSum[i] <= prefixSum[queue[-1]]:
+        for j in range(n + 1):
+            while queue and prefixSum[j] - prefixSum[queue[0]] >= k:
+                ans = min(ans, j - queue.popleft())
+            
+            while queue and prefixSum[j] <= prefixSum[queue[-1]]:
                 queue.pop()
-
-            queue.append(i)
-
+            
+            queue.append(j)
+        
         return -1 if ans == float('inf') else ans
+            
