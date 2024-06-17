80212d48c57d51d5228bd614b6f80945cb94fede
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-06-17 17:47:34 -0400

    Time: 930 ms (31.84%), Space: 30.8 MB (76.03%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
@@ -3,19 +3,22 @@ class Solution:
         n = len(nums)
         prefixSum = [0] * (n + 1)
         for i in range(1, n + 1):
-            # 这里不能用prefixSum[i] += nums[i - 1], prefixSum[i]本身是0，+= nums[i - 1]只会得到nums[i - 1]
             prefixSum[i] = prefixSum[i - 1] + nums[i - 1]
         
-        ans = float('inf')
         queue = deque()
-        for j in range(n + 1):
-            while queue and prefixSum[j] - prefixSum[queue[0]] >= k:
-                ans = min(ans, j - queue.popleft())
+        ans = float('inf')
+        for i in range(n + 1):
+            while queue and prefixSum[i] - prefixSum[queue[0]] >= k:
+                ans = min(ans, i - queue[0])
+                queue.popleft()
             
-            while queue and prefixSum[j] < prefixSum[queue[-1]]:
+            while queue and prefixSum[i] < prefixSum[queue[-1]]:
                 queue.pop()
             
-            queue.append(j)
+            queue.append(i)
         
         return -1 if ans == float('inf') else ans
-            
+        
+        
+        
+        
