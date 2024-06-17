adcdac9fc8734bd7b0883c392e071cbfe9a86169
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-06-17 08:28:44 -0400

    Time: 98 ms (7.96%), Space: 16.7 MB (79.77%) - LeetHub

--- 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.py
+++ 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.py
@@ -1,19 +1,25 @@
 class Solution:
     def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
-        charHm = {}
-        i = 0
+        counter = Counter()
         ans = 0
+        i = 0
         
         for j in range(len(s)):
-            charHm[s[j]] = charHm.get(s[j], 0) + 1
+            # put s[j] into counter
+            # if s[j] is already in counter, it won't affect k
+            # if s[j] is not in counter, we are putting a new value into counter, k--
+            if s[j] not in counter:
+                k -= 1
+            counter[s[j]] += 1
             
-            if len(charHm) <= k:
-                ans = max(ans, j - i + 1)
-                         
-            while len(charHm) > k:
-                charHm[s[i]] -= 1
-                if charHm[s[i]] == 0:
-                    del charHm[s[i]]
+            while k < 0:
+                counter[s[i]] -= 1
+                if counter[s[i]] == 0:
+                    del counter[s[i]]
+                    k += 1
                 i += 1
+            
+            ans = max(ans, j - i + 1)
         
         return ans
+            
