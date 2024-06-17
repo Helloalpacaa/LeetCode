2d6d2e88ac041708c50f9ceefd6c2c65a5db3ee6
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-06-17 08:46:12 -0400

    Time: 103 ms (7.56%), Space: 16.7 MB (79.77%) - LeetHub

--- 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.py
+++ 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.py
@@ -5,23 +5,16 @@ class Solution:
         i = 0
         
         for j in range(len(s)):
-            # put s[j] into counter
-            # if s[j] is already in counter, it won't affect k
-            # if s[j] is not in counter, we are putting a new value into counter, k--
-            if s[j] not in counter:
-                k -= 1
             counter[s[j]] += 1
             
-            # k < 0 说明counter里的elements已经超过了k，需要从i开始移除
-            while k < 0:
+            if len(counter) <= k:
+                ans = max(ans, j - i + 1)
+                
+            while len(counter) > k:
                 counter[s[i]] -= 1
                 if counter[s[i]] == 0:
-                    #这里一定要删掉，不然之后再次遇到同一个character，会认为它还在window里，window里只存放当前valid的window的characters
-                    del counter[s[i]] 
-                    k += 1
+                    del counter[s[i]]
                 i += 1
-            
-            ans = max(ans, j - i + 1)
         
         return ans
             
