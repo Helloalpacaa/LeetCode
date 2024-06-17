4dcc4f869d5ec0e132167510f82d0b1ade017196
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-06-17 08:56:00 -0400

    Time: 448 ms (60.52%), Space: 26.4 MB (62.49%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,30 +1,29 @@
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
-        minQ = collections.deque() # anscending monotonic stack with the min value at the first index
-        maxQ = collections.deque() # descending monotonic stack with the max value at the first index
+        minStack = deque()
+        maxStack = deque()
         i = 0
         ans = 0
         
         for j in range(len(nums)):
-            # maintain a ascending monotonic stack
-            while minQ and nums[j] < minQ[-1]:
-                minQ.pop()
-            # maintain a descending monotonic stack
-            while maxQ and nums[j] > maxQ[-1]:
-                maxQ.pop()
+            while minStack and nums[j] < minStack[-1]:
+                minStack.pop()
+            minStack.append(nums[j])
             
-            minQ.append(nums[j])
-            maxQ.append(nums[j])
+            while maxStack and nums[j] > maxStack[-1]:
+                maxStack.pop()
+            maxStack.append(nums[j])
             
-            # move i to a new start of valid window until pops the element causing the diff
-            while maxQ[0] - minQ[0] > limit:
-                if minQ[0] == nums[i]:
-                    minQ.popleft()
-                if maxQ[0] == nums[i]:
-                    maxQ.popleft()
+            if maxStack[0] - minStack[0] > limit:
+                if nums[i] == maxStack[0]:
+                    maxStack.popleft()
+                if nums[i] == minStack[0]:
+                    minStack.popleft()
                 i += 1
             
             ans = max(ans, j - i + 1)
-            
+        
         return ans
+            
+            
         
