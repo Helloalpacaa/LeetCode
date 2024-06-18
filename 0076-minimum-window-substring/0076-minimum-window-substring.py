03e93f1b1058eb602de7bc9e8f98fb1521e9ef1b
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-06-17 20:20:45 -0400

    Time: 190 ms (27.65%), Space: 17.2 MB (67.74%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,30 +1,26 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        hashmap = {}
-        for c in t:
-            hashmap[c] = hashmap.get(c, 0) + 1
-        size = len(hashmap)
-        i = 0
-        windowSize = float('inf')
+        counter = Counter(t)
+        n = len(counter)
         ans = ""
+        i = 0
+        match = 0
         
         for j in range(len(s)):
-            if s[j] in hashmap:
-                hashmap[s[j]] -= 1
-                if hashmap[s[j]] == 0:
-                    size -= 1
+            if s[j] in counter:
+                counter[s[j]] -= 1
+                if counter[s[j]] == 0:
+                    match += 1
             
-            while size == 0:
-                if windowSize > j - i + 1:
-                    windowSize = j - i + 1
-                    ans = s[i :j + 1]
-                if s[i] in hashmap:
-                    hashmap[s[i]] += 1
-                    if hashmap[s[i]] > 0: # 这个条件一定要有
-                        size += 1
+            while match == n:
+                if not ans or j - i + 1 < len(ans):
+                    ans = s[i : j + 1]
+                    
+                if s[i] in counter:
+                    counter[s[i]] += 1
+                if counter[s[i]] > 0:
+                    match -= 1
                 i += 1
-        
+            
         return ans
-        
-        
-        
+                
