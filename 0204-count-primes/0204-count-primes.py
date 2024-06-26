d858da98c4ac1deaacbeb9e463ae9cb0baf908e6
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-06-26 18:42:22 -0400

    Time: 1192 ms (92.47%), Space: 55.1 MB (86.15%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -6,7 +6,7 @@ class Solution:
         prime = [True] * n
         prime[0] = prime[1] = False
         
-        for i in range(2, int(n ** 0.5) + 1):
+        for i in range(2, int(math.sqrt(n) + 1)):
             if prime[i]:
                 for j in range(i * i, n, i):
                     prime[j] = False
