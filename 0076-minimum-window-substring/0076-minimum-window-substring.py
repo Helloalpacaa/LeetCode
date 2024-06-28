703a4718ca23afa99e24ac4e93dc53f07fd5446b
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-06-28 09:24:43 -0400

    Time: 190 ms (28.39%), Space: 17.3 MB (24.66%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,10 +1,9 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         counter = Counter(t)
-        n = len(counter)
+        match = 0
         ans = ""
         i = 0
-        match = 0
         
         for j in range(len(s)):
             if s[j] in counter:
@@ -12,15 +11,15 @@ class Solution:
                 if counter[s[j]] == 0:
                     match += 1
             
-            while match == n:
-                if not ans or j - i + 1 < len(ans):
-                    ans = s[i : j + 1]
-                    
+            while match == len(counter):
+                if not ans or len(ans) > j - i + 1:
+                    ans = s[i: j + 1]
                 if s[i] in counter:
                     counter[s[i]] += 1
                 if counter[s[i]] > 0:
                     match -= 1
                 i += 1
-            
+        
         return ans
-                
+            
+            
