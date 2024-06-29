9767c9e541510c81156a5c76ed24bc523d2c3bb7
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-06-28 22:41:52 -0400

    Time: 355 ms (7.42%), Space: 17.2 MB (75.55%) - LeetHub

--- 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.py
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.py
@@ -1,18 +1,15 @@
 class Solution:
     def balancedString(self, s: str) -> int:
         counter = Counter(s)
-        ans = float('inf')
         i = 0
-        n = len(s)
+        ans = float('inf')
         
         for j in range(len(s)):
             counter[s[j]] -= 1
             
-            while i < n and all(n / 4 >= counter[c] for c in 'QWER'):
+            while i < len(s) and all(len(s) / 4 >= counter[c] for c in "QWER"):
                 ans = min(ans, j - i + 1)
                 counter[s[i]] += 1
                 i += 1
-                
-        return ans
-                
         
+        return ans
