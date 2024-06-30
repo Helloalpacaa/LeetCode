6465471c5b71a367bc74566c5fff8049fe6292f2
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-06-30 08:39:14 -0400

    Time: 449 ms (54.20%), Space: 26.5 MB (26.93%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,26 +1,28 @@
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
-        minStack = deque()
         maxStack = deque()
-        i = 0
+        minStack = deque()
         ans = 0
+        i = 0
         
         for j in range(len(nums)):
-            while minStack and nums[j] < minStack[-1]:
-                minStack.pop()
-            minStack.append(nums[j])
-            
-            while maxStack and nums[j] > maxStack[-1]:
+            while maxStack and nums[j] > nums[maxStack[-1]]:
                 maxStack.pop()
-            maxStack.append(nums[j])
+            maxStack.append(j)
             
-            if maxStack[0] - minStack[0] > limit:
-                if nums[i] == maxStack[0]:
-                    maxStack.popleft()
-                if nums[i] == minStack[0]:
+            while minStack and nums[j] < nums[minStack[-1]]:
+                minStack.pop()
+            minStack.append(j)
+            
+            while minStack and maxStack and nums[maxStack[0]] - nums[minStack[0]] > limit:
+                if i == minStack[0]:
                     minStack.popleft()
-                i += 1 # Let's consider the window up to the previous element (j-1), which was still valid
+                if i == maxStack[0]:
+                    maxStack.popleft()
+                i += 1
             
             ans = max(ans, j - i + 1)
         
         return ans
+            
+            
