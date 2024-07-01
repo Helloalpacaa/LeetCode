d9de844fe414ae596c085c3b31df94a96ffa77db
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-07-01 11:41:57 -0400

    Time: 641 ms (17.47%), Space: 23.6 MB (50.68%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
@@ -1,8 +1,8 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
         def atMost(k) -> int:
-            i = 0
             ans = 0
+            i = 0
             oddNum = 0
             
             for j in range(len(nums)):
@@ -13,9 +13,9 @@ class Solution:
                     if nums[i] % 2 != 0:
                         oddNum -= 1
                     i += 1
-                    
-                ans += j - i + 1
+                
+                ans += (j - i + 1)
             
             return ans
-                
+        
         return atMost(k) - atMost(k - 1)
