61ec1c1324fc639e1f3762ed240d5fb214929bb6
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-07-03 18:12:29 -0400

    Time: 46 ms (75.30%), Space: 19.5 MB (33.26%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
@@ -4,7 +4,7 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]: 
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
         dummy = ListNode(0, head)
         curr = dummy
         
@@ -15,5 +15,3 @@ class Solution:
                 curr = curr.next
         
         return dummy.next
-        
-        
