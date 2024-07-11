f9df679872e6ab84c0ec90a5132dbb0b7b246bc5
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-07-11 00:20:53 -0400

    Time: 16 ms (48.59%), Space: 11.7 MB (53.67%) - LeetHub

--- 0925-long-pressed-name/0925-long-pressed-name.py
+++ 0925-long-pressed-name/0925-long-pressed-name.py
@@ -1,10 +1,16 @@
-class Solution:
-    def isLongPressedName(self, name: str, typed: str) -> bool:
+class Solution(object):
+    def isLongPressedName(self, name, typed):
+        """
+        :type name: str
+        :type typed: str
+        :rtype: bool
+        """
         i = 0
+        
         for j in range(len(typed)):
             if i < len(name) and name[i] == typed[j]:
                 i += 1
             elif j == 0 or typed[j] != typed[j - 1]:
                 return False
-            
+        
         return i == len(name)
