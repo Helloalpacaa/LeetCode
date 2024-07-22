d0dee40c0f4c63e4be8c3f066868b4aff35367ee
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-07-22 08:18:40 -0400

    Time: 45 ms (6.19%), Space: 16.6 MB (7.1%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.py
+++ 0257-binary-tree-paths/0257-binary-tree-paths.py
@@ -6,21 +6,16 @@
 #         self.right = right
 class Solution:
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
-        if root is None:
-            return []
+        def traversal(node: Optional[TreeNode], path: str) -> None:
+            if node.left is None and node.right is None:
+                self.ans.append(path)
+                return
+            
+            if node.left:
+                traversal(node.left, path + "->" + str(node.left.val))
+            if node.right:
+                traversal(node.right, path + "->" + str(node.right.val))
         
-        ans = []
-        path = ""
-        self.traversal(root, path, ans)
-        return ans
-    
-    def traversal(self, node: Optional[TreeNode], path: str, ans: List[str]) -> None:
-        if node.left is None and node.right is None:
-            ans.append(path + str(node.val))
-            return
-        
-        if node.left:
-            self.traversal(node.left, path + str(node.val) + "->", ans)
-        if node.right:
-            self.traversal(node.right, path + str(node.val) + "->", ans)
-        
+        self.ans = []
+        traversal(root, str(root.val))
+        return self.ans
