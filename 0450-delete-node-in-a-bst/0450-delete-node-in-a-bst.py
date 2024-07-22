d84f6887c5d6a85dbcc1e8a69719d15503c22509
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-07-21 22:48:54 -0400

    Time: 47 ms (95.36%), Space: 19.9 MB (7.63%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
@@ -8,25 +8,19 @@ class Solution:
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
         if root is None:
             return None
-        
-        if root.val > key:
-            root.left = self.deleteNode(root.left, key)
-        elif root.val < key:
+
+        if root.val < key:
             root.right = self.deleteNode(root.right, key)
+        elif root.val > key:
+            root.left = self.deleteNode(root.left, key)
         else:
-            # 1. No left child
-            if root.left is None:
-                return root.right
-            # 2. No right child
-            elif root.right is None:
+            if root.right is None:
                 return root.left
-            # 3. Have both left and right child
-            else:
-                # 去找右边的最小值，用最小值作为root的值，再去删掉这个最小值的node
-                successor = root.right
-                while successor.left:
-                    successor = successor.left
-                root.val = successor.val
-                root.right = self.deleteNode(root.right, successor.val)
+            curr = root.right
+            while curr.left:
+                curr = curr.left
+            root.val = curr.val
+            root.right = self.deleteNode(root.right, curr.val)
         
         return root
+            
