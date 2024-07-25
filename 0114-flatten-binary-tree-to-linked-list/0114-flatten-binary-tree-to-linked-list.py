03ff803e316927f872a2d8294379eebd549b2b23
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-07-24 20:43:07 -0400

    Time: 36 ms (74.2%), Space: 16.8 MB (26.19%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
@@ -11,12 +11,10 @@ class Solution:
         """
         if root is None:
             return
-
-        # 先把root的左边拉成一条直线，再把root的右边拉成一条直线
+        
         self.flatten(root.left)
         self.flatten(root.right)
 
-        # 先从3开始，3是一个点，就是一条直线，再到4，再返回到上一层2，把234连成一条直线
         left = root.left
         right = root.right
 
@@ -26,7 +24,6 @@ class Solution:
         p = root
         while p.right:
             p = p.right
+        
         p.right = right
 
-
-        
