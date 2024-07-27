7bfa1f70cf56da6dff483d15a16d2ec631eb1340
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-07-27 16:29:01 -0400

    Time: 148 ms (75.46%), Space: 21.7 MB (65.31%) - LeetHub

--- 1245-tree-diameter/1245-tree-diameter.py
+++ 1245-tree-diameter/1245-tree-diameter.py
@@ -1,25 +1,27 @@
 class Solution:
     def treeDiameter(self, edges: List[List[int]]) -> int:
-        graph = defaultdict(list)
+        self.graph = defaultdict(list)
         for u, v in edges:
-            graph[u].append(v)
-            graph[v].append(u)
+            self.graph[u].append(v)
+            self.graph[v].append(u)
         
-        def dfs(parent: int, child: int) -> Tuple[int, int]:
+        self.diameter = 0
+
+        def traversal(parent: int, child: int) -> int:
             maxDepth1, maxDepth2 = 0, 0
 
-            for nextNode in graph[child]:
-                if nextNode != parent:
-                    depth = dfs(child, nextNode)[0]
+            for grandchild in self.graph[child]:
+                if grandchild != parent:
+                    depth = traversal(child, grandchild)
                     if depth > maxDepth1:
                         maxDepth1, maxDepth2 = depth, maxDepth1
                     elif depth > maxDepth2:
                         maxDepth2 = depth
-                    self.diameter = max(self.diameter, maxDepth1 + maxDepth2)
+
+            self.diameter = max(self.diameter, maxDepth1 + maxDepth2)
             
-            return (maxDepth1 + 1, maxDepth2 + 1)
+            return maxDepth1 + 1
         
-        self.diameter = 0
-        dfs(-1, 0)
+        traversal(-1, 0)
         return self.diameter
         
