3a34f6859e7d6fb5fc9570cdf8ae13e73a919ae1
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-07-31 19:42:20 -0400

    Time: 35 ms (81.65%), Space: 18.1 MB (31.32%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.py
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.py
@@ -6,7 +6,8 @@
 #         self.right = right
 class Solution:
     def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
-
+        
+        # 前置遍历，在到达每一个节点时先更新从root到这个节点的最大值和最小值，然后把maxDiff向上返回
         def traversal(node: Optional[TreeNode], minVal: int, maxVal: int) -> int:
             if node is None:
                 return maxVal - minVal
@@ -14,6 +15,9 @@ class Solution:
             minVal = min(minVal, node.val)
             maxVal = max(maxVal, node.val)
 
-            return max(traversal(node.left, minVal, maxVal), traversal(node.right, minVal, maxVal))
+            left = traversal(node.left, minVal, maxVal)
+            right = traversal(node.right, minVal, maxVal)
+
+            return max(left, right, maxVal - minVal)
         
         return traversal(root, root.val, root.val)
