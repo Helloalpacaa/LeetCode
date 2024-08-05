7cda975fdfcebc8a35afccef4dfe10db2c1d6328
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-08-05 00:27:54 -0400

    Time: 14 ms (94.23%), Space: 91.6 MB (96.69%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,21 +1,26 @@
 class Solution {
-    private List<Integer> path = new LinkedList<>();
-    private List<List<Integer>> ans = new ArrayList<>();
-    
+    List<List<Integer>> ans;
+    LinkedList<Integer> path;
     public List<List<Integer>> combine(int n, int k) {
-        backtracking(n, k, 1);
+        ans = new ArrayList<>();
+        path = new LinkedList<>();
+        backTracking(n, k, 1);
         return ans;
     }
-    
-    private void backtracking(int n, int k, int startIdx) {
+
+    private void backTracking(int n, int k, int index) {
         if (path.size() == k) {
             ans.add(new ArrayList<>(path));
             return;
         }
-        
-        for (int i = startIdx; i <= n - (k - path.size()) + 1; i++) {
+
+        // 已经选择的元素个数：path.size()
+        // 还需要的元素个数为：k - path.size()
+        // 在集合n中至多要从该起始位置：n - (k - path.size()) + 1，开始遍历
+        // 例子1就不会有起始index为i = 4的情况，因为4后面没有数字，无论如何也凑不出一对组合
+        for (int i = index; i <= n - (k - path.size()) + 1; i++) {
             path.add(i);
-            backtracking(n, k, i + 1);
+            backTracking(n, k, i + 1);
             path.removeLast();
         }
     }
