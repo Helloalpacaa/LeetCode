29ad1701579c12980dea84acd4a0ad75bf08d94b
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-08-08 06:26:50 -0400

    Time: 79 ms (24.69%), Space: 16.9 MB (67.84%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,39 +1,36 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
+        board = [["."] * n for _ in range(n)]
         ans = []
-        board = [["." for _ in range(n)] for _ in range(n)]
-    
-        def isValid(row: int, col: int) -> bool:
-            # check column
-            for i in range(row):
-                if board[i][col] == 'Q':
-                    return False
 
-            # check upper-left diagonal
-            for i, j in zip(range(row - 1, -1, -1), range(col - 1, -1, -1)):
-                if board[i][j] == 'Q':
+        def isValid(row: int, col: int) -> bool:
+            # check row
+            for i in range(row):
+                if board[i][col] == "Q":
                     return False
             
-            # check upper-right diagonal
+            # check left upper
+            for i, j in zip(range(row - 1, -1, -1), range(col - 1, -1, -1)):
+                if board[i][j] == "Q":
+                    return False
+            
+            # check right upper
             for i, j in zip(range(row - 1, -1, -1), range(col + 1, n, 1)):
-                if board[i][j] == 'Q':
+                if board[i][j] == "Q":
                     return False
             
             return True
         
         def backtracking(row: int) -> None:
             if row == n:
-                ans.append([''.join(row) for row in board])
+                ans.append(["".join(row) for row in board])
                 return
             
-            for col in range(n):
-                if isValid(row, col):
-                    board[row][col] = 'Q'
+            for i in range(n):
+                if isValid(row, i):
+                    board[row][i] = "Q"
                     backtracking(row + 1)
-                    board[row][col] = '.'
+                    board[row][i] = "."
         
         backtracking(0)
         return ans
-        
-
-            
