36ba770d8bb2c6a7517e24d9626886535540cf03
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-08-08 06:48:35 -0400

    Time: 260 ms (53.98%), Space: 16.6 MB (62.37%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.py
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -3,21 +3,15 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        def isValid(row: int, col: int, val: chr) -> bool:
-            # check row
+        def isValid(row: int, col: int, val: str) -> bool:
             for i in range(9):
-                if board[row][i] == val:
+                if board[row][i] == val or board[i][col] == val:
                     return False
             
-            # check col
-            for j in range(9):
-                if board[j][col] == val:
-                    return False
-            
-            # check 3 * 3 subgrid
-            start_row, start_col = 3 * (row // 3), 3 * (col // 3)
-            for i in range(start_row, start_row + 3):
-                for j in range(start_col, start_col + 3):
+            start_row = (row // 3) * 3
+            start_col = (col // 3) * 3
+            for i in range(start_row, start_row + 3, 1):
+                for j in range(start_col, start_col + 3, 1):
                     if board[i][j] == val:
                         return False
             
@@ -26,25 +20,17 @@ class Solution:
         def backtracking() -> bool:
             for i in range(9):
                 for j in range(9):
-                    # 如果是数字，去下一个格子
-                    if board[i][j] != '.':
+                    if board[i][j] != ".":
                         continue
-
-                    # 如果是‘.'，看哪个数字可以放在这里
+                    
                     for val in "123456789":
                         if isValid(i, j, val):
                             board[i][j] = val
-                            # 找到可以放在这里的数字后，继续当前棋盘
-                            # 如果下一个格子“123456789”都不valid，会return False，那么当前格子的值会直接回溯，去测试下一个数字是否valid
-                            # 如果找到一组合适立刻返回
                             if backtracking():
                                 return True
-                            board[i][j] = '.'
-                    
-                    # 如果试了9个数字都不valid，说明这个格子没有valid的值，return False结束
+                            board[i][j] = "."
                     return False
             
-            # 如果不曾return False，就return True
             return True
         
         backtracking()
