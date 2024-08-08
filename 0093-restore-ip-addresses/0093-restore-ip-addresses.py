6f877b98f06255bc1dd6b0f1350703a697e18860
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-08-07 23:44:09 -0400

    Time: 36 ms (76.94%), Space: 16.4 MB (97.33%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -2,24 +2,23 @@ class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
         ans = []
 
-        def isValid(segment: str) -> bool:
-            return len(segment) == 1 or (segment[0] != '0' and int(segment) <= 255)
+        def isValid(s: str) -> bool:
+            return len(s) == 1 or (len(s) > 1 and s[0] != '0' and int(s) <= 255)
 
-        def backtracking(start: int, parts: List[str]) -> None:
-            if len(parts) == 4 and start == len(s):
-                ans.append('.'.join(parts))
+        def backtracking(parts: List[str], index: int) -> None:
+            if len(parts) == 4 and index == len(s):
+                ans.append(".".join(parts[:]))
                 return
             
-            if len(parts) == 4 or start == len(s):
+            # 这步优化不能少
+            if len(parts) == 4 or index == len(s):
                 return
-            
-            for i in range(start, min(start + 3, len(s))):
-                segment = s[start: i + 1]
-                if isValid(segment):
-                    parts.append(segment)
-                    backtracking(i + 1, parts)
+
+            for i in range(len(s)):
+                if isValid(s[index: i + 1]):
+                    parts.append(s[index: i + 1])
+                    backtracking(parts, i + 1)
                     parts.pop()
         
-        backtracking(0, [])
+        backtracking([], 0)
         return ans
-
