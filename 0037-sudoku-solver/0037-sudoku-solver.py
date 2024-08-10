48aec0d205af0d251d66e6e0d0b398e21a43671b
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-08-09 23:11:31 -0400

    Time: 282 ms (46.03%), Space: 16.5 MB (63.61%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.py
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -3,15 +3,18 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
+        
         def isValid(row: int, col: int, val: str) -> bool:
+            # check row
             for i in range(9):
                 if board[row][i] == val or board[i][col] == val:
                     return False
             
+            # check 3 * 3
             start_row = (row // 3) * 3
             start_col = (col // 3) * 3
-            for i in range(start_row, start_row + 3, 1):
-                for j in range(start_col, start_col + 3, 1):
+            for i in range(start_row, start_row + 3):
+                for j in range(start_col, start_col + 3):
                     if board[i][j] == val:
                         return False
             
@@ -20,12 +23,12 @@ class Solution:
         def backtracking() -> bool:
             for i in range(9):
                 for j in range(9):
-                    if board[i][j] != ".":
+                    if board[i][j].isdigit():
                         continue
                     
-                    for val in "123456789":
-                        if isValid(i, j, val):
-                            board[i][j] = val
+                    for char in "123456789":
+                        if isValid(i, j, char):
+                            board[i][j] = char
                             if backtracking():
                                 return True
                             board[i][j] = "."
@@ -34,3 +37,4 @@ class Solution:
             return True
         
         backtracking()
+                        
