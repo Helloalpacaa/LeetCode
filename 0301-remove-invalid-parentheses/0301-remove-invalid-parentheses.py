2525529589c7782bb44b592ff7e46c4b2e363f41
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-08-10 00:28:18 -0400

    Time: 192 ms (49.14%), Space: 18.2 MB (12.83%) - LeetHub

--- 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.py
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.py
@@ -1,28 +1,12 @@
 class Solution:
     def removeInvalidParentheses(self, s: str) -> List[str]:
-        ans = []
-
-        def isValid(s: str) -> bool:
-            count = 0
-            for char in s:
-                if char == "(":
-                    count += 1
-                elif char == ")":
-                    count -= 1
-                
-                # 说明有一个“）”前面没有对应的“（”
-                if count < 0:
-                    return False
-            
-            # 遍历完S后，count需要等于0
-            return count == 0
         
-        def getMinRemoval(s: str) -> int:
-            left = right = 0
-            for char in s:
-                if char == "(":
+        def getMinRemove(s: str) -> int:
+            left, right = 0, 0
+            for i in range(len(s)):
+                if s[i] == "(":
                     left += 1
-                elif char == ")":
+                elif s[i] == ")":
                     if left > 0:
                         left -= 1
                     else:
@@ -30,31 +14,36 @@ class Solution:
             
             return left + right
         
-        # BFS
-        min_removal = getMinRemoval(s)
-        queue = deque([s])
-        visited = set([s]) # 去重
-        level = 0 # 用来记录已经移除了多少个char
+        def isValid(s: str) -> bool:
+            left, right = 0, 0
+            for i in range(len(s)):
+                if s[i] == "(":
+                    left += 1
+                elif s[i] == ")":
+                    if left > 0:
+                        left -= 1
+                    else:
+                        return False
+            return left == 0
+        
+        min_removal = getMinRemove(s)
+        visited = set()
+        queue = deque()
+        queue.append(s)
+        level = 0
 
         while queue:
             if level == min_removal:
                 return [x for x in queue if isValid(x)]
-            
+
             for _ in range(len(queue)):
-                curr = queue.popleft() # get the string, example - "()())()"
+                curr = queue.popleft()
                 for i in range(len(curr)):
-                    if curr[i] in '()':
-                        new_str = curr[:i] + curr[i+1:]
-                        # 不能在这里check是否valid，因为有可能先加入一个invalid的str，再通过删除变成valid
-                        if new_str not in visited:
-                            queue.append(new_str)
-                            visited.add(new_str)
+                    new_str = curr[:i] + curr[i+1:]
+                    if new_str not in visited:
+                        queue.append(new_str)
+                    visited.add(new_str)
             
             level += 1
-        
+
         return [""]
-
-
-        
-
-        
