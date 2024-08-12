6ea3ed3e2bc2fdfc4c71552eab76fd971c3c6d32
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-08-12 06:52:13 -0400

    Time: 52 ms (42.25%), Space: 16.6 MB (83.62%) - LeetHub

--- 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.py
+++ 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.py
@@ -1,7 +1,14 @@
 class Solution:
     def largestSumAfterKNegations(self, nums: List[int], k: int) -> int:
-        for _ in range(k):
-            min_index = nums.index(min(nums))
-            nums[min_index] = -nums[min_index]
+        # 1. sort the array in ascending order
+        nums.sort()
+
+        # 2. flip the negative numbers
+        i = 0
+        while i < len(nums) and i < k and nums[i] < 0:
+            nums[i] = -nums[i]
+            i += 1
         
-        return sum(nums)
+        # 3. count the sum, 不管k多大，如果是偶数把最小值flip两遍，如果是奇数把最小值flip一遍
+        # 乘以2是因为之前加过，所以要减掉两次
+        return sum(nums) - (k - i) % 2 * min(nums) * 2
