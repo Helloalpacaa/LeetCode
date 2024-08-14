267320716092f10ee321782acae5aabf43c759e0
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-08-14 07:46:42 -0400

    Time: 104 ms (66.46%), Space: 17.6 MB (69.61%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,15 +1,18 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        jumps = 0
-        furthest = 0
+        if len(nums) <= 1:
+            return 0
+            
+        steps = 0
         end = 0
+        furthest = 0
 
-        # we are jumping to the furthest point in the current coverage
-        # when we reach the current end, trigger another jump
-        for i in range(len(nums) - 1):
-            furthest = max(furthest, nums[i] + i)
-            if end == i:
-                jumps += 1
+        for i in range(len(nums)):
+            furthest = max(furthest, i + nums[i])
+            if i == end:
+                steps += 1
                 end = furthest
+                if end >= len(nums) - 1:
+                    break
         
-        return jumps
+        return steps
