c3cae7d1a165dc4ebe55c6fa646682cdc527e7f1
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-08-14 23:10:58 -0400

    Time: 44 ms (46.47%), Space: 16.7 MB (67.91%) - LeetHub

--- 0968-binary-tree-cameras/0968-binary-tree-cameras.py
+++ 0968-binary-tree-cameras/0968-binary-tree-cameras.py
@@ -8,23 +8,25 @@ class Solution:
     def minCameraCover(self, root: Optional[TreeNode]) -> int:
         self.ans = 0
 
+        # 0: leaf
+        # 1: parents of leaves, should have camera
+        # 2: covered
+        # 我们只在leaf的parent放置camera
         def dfs(node: Optional[TreeNode]) -> int:
-            # 0: leaf node
-            # 1: parent of leaf node, install camara here
-            # 2: covered, no need to install camaera here
             if node is None:
                 return 2
             
             left = dfs(node.left)
             right = dfs(node.right)
-
             if left == 0 or right == 0:
                 self.ans += 1
                 return 1
-
+            
             if left == 1 or right == 1:
                 return 2
-
+            
+            # left == 2 or right == 2的情况不需要额外handle，这是node就相当于leaf，直接return 0
             return 0
-        
+
+        # adds 1 to the total if the root itself needs a camera
         return (dfs(root) == 0) + self.ans
