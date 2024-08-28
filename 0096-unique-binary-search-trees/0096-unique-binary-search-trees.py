bc1e7069ca447f7dc3570f8ddb3cdec0c45bfc2c
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-08-28 11:21:28 -0400

    Time: 33 ms (68.42%), Space: 16.5 MB (20.76%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -4,6 +4,8 @@ class Solution:
         dp[0] = 1
         dp[1] = 1
 
+        # n = 1时，只有一种BST
+        # n = 2时，①root为1，BST数量 = 左子树的BST数量 * 右子树的BST数量 ②root为2。。。
         for i in range(2, n + 1):
             for j in range(1, i + 1):
                 dp[i] += dp[j - 1] * dp[i - j]
