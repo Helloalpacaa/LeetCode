87cc6066464400b964acad79cd7a6e02f61d42e0
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-08-30 07:01:39 -0400

    Time: 57 ms (96.7%), Space: 16.6 MB (95.27%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,23 +1,24 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        # left + right = sum
-        # left - right = target
-        if (sum(nums) + target) % 2 != 0:
-            return 0
-        
-        if sum(nums) + target < 0:
+        # left代表未经被改变符号的部分
+        # right代表符号被改变了的部分
+        # left + right = target
+        # left - right = sum(nums)
+        if (target + sum(nums)) % 2 != 0:
             return 0
 
-        left = (sum(nums) + target) / 2
-    
-        # dp[j]: 容量为j的背包，有多少种方式填满
-        left = int(left)
+        left = (target + sum(nums)) // 2
+        if left < 0:
+            return 0
+            
+        # dp[i]: 装满背包容量为i有多少种方法
         dp = [0] * (left + 1)
         dp[0] = 1
+
         for num in nums:
-            for j in range(left, num - 1, -1):
-                dp[j] += dp[j - num]
-        
-        print(dp)
+            for i in range(left, num - 1, -1):
+                dp[i] += dp[i - num]
         
         return dp[left]
+
+        
