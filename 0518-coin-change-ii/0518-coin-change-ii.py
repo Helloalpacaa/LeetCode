b1cf188c8dd5084966b43d009ea0637ecadb251b
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-09-01 09:04:23 -0400

    Time: 91 ms (87.72%), Space: 16.7 MB (76%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -5,8 +5,7 @@ class Solution:
         dp[0] = 1
 
         for coin in coins:
-            for i in range(1, amount + 1):
-                if i >= coin:
-                    dp[i] += dp[i - coin]
+            for i in range(coin, amount + 1):
+                dp[i] += dp[i - coin]
         
         return dp[amount]
