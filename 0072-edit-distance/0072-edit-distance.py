e566fc05752e87155c04ba28e74890cbf4f66882
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-09-05 10:59:43 -0400

    Time: 94 ms (73.1%), Space: 20.2 MB (47.46%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -12,7 +12,7 @@ class Solution:
         for i in range(m):
             for j in range(n):
                 if word1[i] == word2[j]:
-                    dp[i + 1][j + 1] = min(dp[i][j], min(dp[i][j + 1], dp[i + 1][j]) + 2)
+                    dp[i + 1][j + 1] = dp[i][j]
                 else:
                     dp[i + 1][j + 1] = min(dp[i][j], dp[i + 1][j], dp[i][j + 1]) + 1
         
