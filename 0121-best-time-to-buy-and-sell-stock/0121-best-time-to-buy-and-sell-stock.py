af5d78fb4e2d62d7217fc79792ed03820cf09a9b
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-09-06 18:51:25 -0400

    Time: 1084 ms (5.01%), Space: 31.6 MB (5.59%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,11 +1,24 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         # 贪心：取最左最小值，取最右最大值，那么得到的差就是最大利润
-        profit = 0
-        minValue = float('inf')
+        # profit = 0
+        # minValue = float('inf')
 
-        for price in prices:
-            minValue = min(minValue, price)
-            profit = max(profit, price - minValue)
+        # for price in prices:
+        #     minValue = min(minValue, price)
+        #     profit = max(profit, price - minValue)
 
-        return profit
+        # return profit
+
+        # dp[i][0]: 第i天持有股票的利润
+        # dp[i][1]: 第i天不持有股票的利润
+        n = len(prices)
+        dp = [[0] * 2 for _ in range(n)]
+        dp[0][0] = -prices[0]
+
+        for i in range(1, n):
+            dp[i][0] = max(dp[i - 1][0], - prices[i])
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i])
+        
+        return dp[n - 1][1]
+
