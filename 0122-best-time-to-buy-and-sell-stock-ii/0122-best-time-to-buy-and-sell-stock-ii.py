9923f893ed05f57a10b7ad5fa3dfb00c392df62c
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-09-06 19:30:42 -0400

    Time: 55 ms (73.29%), Space: 18 MB (12.66%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,13 +1,22 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
-        minVal = prices[0]
-        profit = 0
+        # dp[i][0]: 第i天持有股票的最大profit
+        # dp[i][1]: 第i天不持有股票的最大profit
+        # dp = [[0] * 2 for _ in range(n)]
+        # dp[0][0] = -prices[0]
+
+        # for i in range(1, n):
+        #     dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] - prices[i])
+        #     dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i])
+        
+        # return dp[n - 1][1]
+
+        hold = -prices[0]
+        not_hold = 0
 
         for i in range(1, n):
-            if prices[i] > minVal:
-                profit += prices[i] - minVal
-            
-            minVal = prices[i]
-        
-        return profit
+            hold = max(hold, not_hold - prices[i])
+            not_hold = max(not_hold, hold + prices[i])
+
+        return not_hold
