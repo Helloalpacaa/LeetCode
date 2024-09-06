2fe0b7af89f7916931d9368282792d02c0a3323d
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-09-06 18:52:36 -0400

    Time: 811 ms (7.23%), Space: 27.4 MB (38.3%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -12,13 +12,22 @@ class Solution:
 
         # dp[i][0]: 第i天持有股票的利润
         # dp[i][1]: 第i天不持有股票的利润
-        n = len(prices)
-        dp = [[0] * 2 for _ in range(n)]
-        dp[0][0] = -prices[0]
+        # n = len(prices)
+        # dp = [[0] * 2 for _ in range(n)]
+        # dp[0][0] = -prices[0]
 
-        for i in range(1, n):
-            dp[i][0] = max(dp[i - 1][0], - prices[i])
-            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i])
+        # for i in range(1, n):
+        #     dp[i][0] = max(dp[i - 1][0], - prices[i])
+        #     dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i])
         
-        return dp[n - 1][1]
+        # return dp[n - 1][1]
+
+        hold = -prices[0]
+        not_hold = 0
+
+        for i in range(1, len(prices)):
+            hold = max(hold, -prices[i])
+            not_hold = max(not_hold, hold + prices[i])
+
+        return not_hold
 
