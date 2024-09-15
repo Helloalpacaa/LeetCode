e6168316688679370cfab7219aed705d4c5caa2f
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-09-15 07:31:22 -0400

    Time: 56 ms (12.11%), Space: 18.1 MB (46.37%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.py
+++ 0337-house-robber-iii/0337-house-robber-iii.py
@@ -7,20 +7,16 @@
 class Solution:
     def rob(self, root: Optional[TreeNode]) -> int:
         
-        # return a tuple(rob, not_rob)
         def dfs(node: Optional[TreeNode]) -> (int, int):
             if node is None:
-                return 0, 0
-            
-            # 后序遍历，因为我们要先拿到left和right的结果
+                return (0, 0)
+
             left = dfs(node.left)
             right = dfs(node.right)
 
-            # 抢当前node，那么就不能抢left child and right child
             rob = node.val + left[1] + right[1]
-            # 不抢当前node，就可以抢left child and right child，可以选择抢或者不抢，取更大的值
             not_rob = max(left) + max(right)
 
             return (rob, not_rob)
         
-        return max(dfs(root))
+        return max(dfs(root)[0], dfs(root)[1])
