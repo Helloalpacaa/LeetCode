b4109edb5232dc5ed6debec96dc07c29f2c80625
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-09-16 21:08:10 -0400

    Time: 654 ms (49.89%), Space: 18.3 MB (73.16%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -2,7 +2,7 @@ class Solution:
     def minInsertions(self, s: str) -> int:
         n = len(s)
         dp = [[0] * (n + 1) for _ in range(n + 1)]
-        s_reverse = s[::-1]
+        s_reverse = ''.join(reversed(s))
 
         for i in range(n):
             for j in range(n):
