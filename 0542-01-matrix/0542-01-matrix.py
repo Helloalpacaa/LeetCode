18443702a44070a72c294528c628d3c8afd35424
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-09-18 07:04:07 -0400

    Time: 853 ms (5.03%), Space: 21.1 MB (8.08%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -2,52 +2,27 @@ class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         m = len(mat)
         n = len(mat[0])
-        # memo = {}
-        
-        # def dfs(i: int, j: int) -> int:
-        #     if i < 0 or i >= m or j < 0 or j >= n:
-        #         return float('inf')
-            
-        #     if mat[i][j] == 0:
-        #         return 0
-            
-        #     key = i, j
-        #     if key in memo:
-        #         return memo[key]
-
-        #     memo[key] = float('inf')
-
-        #     memo[key] = 1 + min(dfs(i, j - 1), dfs(i, j + 1), dfs(i - 1, j), dfs(i + 1, j))
-
-        #     return memo[key]
-        
-        # ans = [[0] * n for _ in range(m)]
-        # for i in range(m):
-        #     for j in range(n):
-        #         ans[i][j] = dfs(i, j)
-        
-        # return ans
-        
-        #这题不适合用DFS做，哪怕用一个Dict存储所有visit过的cell，最坏的情况它可能还需要不断revisit这些计算过的cell
-        #这种求最短路程的题，考虑用BFS，直接从值为0的cell出发向外扩展，就保证了它是最小路径
         ans = [[float('inf')] * n for _ in range(m)]
-        queue = deque()
+        queue = deque([])
 
         for i in range(m):
             for j in range(n):
                 if mat[i][j] == 0:
                     ans[i][j] = 0
-                    queue.append((i, j))
-
-        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+                    queue.append([i, j])
+        
+        directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+        visited = set([])
 
         while queue:
-            x, y = queue.popleft()
+            i, j = queue.popleft()
             for direction in directions:
-                nx, ny = x + direction[0], y + direction[1]
-                if 0 <= nx < m and 0 <= ny < n and ans[nx][ny] > ans[x][y] + 1:
-                    ans[nx][ny] = ans[x][y] + 1
-                    queue.append((nx, ny))
-        
-        return ans
+                next_i = i + direction[0]
+                next_j = j + direction[1]
+                key = next_i, next_j
+                if next_i >= 0 and next_i < m and next_j >= 0 and next_j < n and key not in visited:
+                    ans[next_i][next_j] = min(ans[next_i][next_j], ans[i][j] + 1)
+                    queue.append([next_i, next_j])
+            visited.add((i, j))
 
+        return ans
