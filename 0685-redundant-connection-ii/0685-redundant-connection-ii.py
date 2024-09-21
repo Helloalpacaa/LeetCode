83da96514f82f7791073a5d385f29d5540069e46
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-09-21 17:40:58 -0400

    Time: 60 ms (62.62%), Space: 17.2 MB (26.7%) - LeetHub

--- 0685-redundant-connection-ii/0685-redundant-connection-ii.py
+++ 0685-redundant-connection-ii/0685-redundant-connection-ii.py
@@ -1,40 +1,53 @@
 class Solution:
     def findRedundantDirectedConnection(self, edges: List[List[int]]) -> List[int]:
+        # invalid有两种情况
+        # 1. 一个node有两个roots，也就是一个node有两个indegrees，需要砍掉一条边，使得另外的还是能组成一个valid tree
+        # 2. 形成了一个circle，需要砍掉形成circle的那条边（更后面出现的那条）
         n = len(edges)
-        parent = list(range(n + 1))
+
+        # check for a node with two parents
+        indegree = [0] * (n + 1)
+        for u, v in edges:
+            indegree[v] += 1
         
-        def find(x):
-            if parent[x] != x:
-                parent[x] = find(parent[x])
-            return parent[x]
-        
-        def union(x, y):
-            parent[find(x)] = find(y)
-        
-        # Check for a node with two parents
-        child_parent = {}
+        # add the nodes with two indegrees into candidates, if there are any there should be two
+        # with the second one has a higher priority to be returned
         candidate1, candidate2 = None, None
         for u, v in edges:
-            if v in child_parent:
-                candidate1 = child_parent[v]
-                candidate2 = [u, v]
-                break
-            child_parent[v] = [u, v]
+            if indegree[v] == 2:
+                candidate1 = candidate2
+                candidate2 = u, v  
+        # candidate1在edges中在candidate2的前面，如果要return也是return candidate2，所以我们等会会先check不用candidate2去组成graph的情况
         
-        # If no node has two parents, find the edge that creates a cycle
-        if not candidate1:
-            for u, v in edges:
-                if find(u) == find(v):
-                    return [u, v]
-                union(u, v)
+        # union find的base code
+        father = [i for i in range(n + 1)]
         
-        # If a node has two parents, check which edge can be removed
-        else:
+        def join(u: int, v: int) -> None:
+            u = find(u)
+            v = find(v)
+            if u == v:
+                return
+            father[v] = u
+        
+        def find(u: int) -> int:
+            if u == father[u]:
+                return u
+            father[u] = find(father[u])
+            return father[u]
+        
+        # 如果有indegrees为2的nodes，check如果不用candidate 2来组成graph，是否还是一个valid的graph
+        if candidate2:
             for u, v in edges:
-                if [u, v] == candidate2:
+                if (u, v) == candidate2:
                     continue
-                union(u, v)
+                join(u, v)
             
             if find(candidate2[0]) == find(candidate2[1]):
                 return candidate2
-            return candidate1
+            else:
+                return candidate1
+        else:
+            for u, v in edges:
+                if find(u) == find(v):
+                    return (u, v)
+                join(u, v)
