8f902fd2d12c5fc43497c30dd5b8d38e26b583da
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-10-07 20:17:58 -0400

    Time: 104 ms (26.84%), Space: 18.5 MB (29.04%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,51 +1,34 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # DFS 解法
-        # graph = [[] for _ in range(numCourses)]
-        # for course, pre in prerequisites:
-        #     graph[course].append(pre)
-        
-        # exploring = set()
-        # visited = set()
-
-        # def dfs(course) -> bool:
-        #     if course in exploring:
-        #         return True # 有环
-            
-        #     if course in visited:
-        #         return False # 没有环
-            
-        #     exploring.add(course)
-
-        #     for pre in graph[course]:
-        #         if dfs(pre):
-        #             return True # 有环
-            
-        #     # 没有遇到有环的情况，结束exploring，放进visited
-        #     exploring.remove(course) 
-        #     visited.add(course)
-
-        #     return False
-        
-        # for course, pre in prerequisites:
-        #     if dfs(pre):
-        #         return False
-        
-        # return True
-
-        # BFS解法
-        graph = [[] for _ in range(numCourses)]
-        indegree = [0] * numCourses
+        preList = [[] for _ in range(numCourses)]
         for course, pre in prerequisites:
-            graph[pre].append(course)
-            indegree[course] += 1
-        
-        bfs = [i for i in range(numCourses) if indegree[i] == 0]
+            preList[course].append(pre)
 
-        for i in bfs:
-            for j in graph[i]:
-                indegree[j] -= 1
-                if indegree[j] == 0:
-                    bfs.append(j)
+        exploring = set()
+        visited = set()
+
+        def dfs(course) -> bool:
+            if course in exploring:
+                return True
+            
+            if course in visited:
+                return False
+            
+            exploring.add(course)
+            
+            for pre in preList[course]:
+                if dfs(pre):
+                    return True
+            
+            exploring.remove(course)
+            visited.add(course)
+
+            return False
+            
+
+
+        for course, pre in prerequisites:
+            if dfs(course):
+                return False
         
-        return len(bfs) == numCourses
+        return True
