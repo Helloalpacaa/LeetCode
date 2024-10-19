e8ec370dc984f44efa228525abc4215dd42298d7
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-10-19 10:45:49 -0400

    Time: 2917 ms (92.59%), Space: 218.2 MB (79.8%) - LeetHub

---
+++ 3320-count-the-number-of-winning-sequences/3320-count-the-number-of-winning-sequences.py
@@ -0,0 +1,36 @@
+class Solution:
+    def countWinningSequences(self, s: str) -> int:
+        MOD = 10**9 + 7
+        n = len(s)
+        s = ['FWE'.find(c) for c in s] # convert string to list for indices
+
+        # dp[i][j] is a Counter where:
+        # - i is the current round
+        # - j is Bob's current move(0: F, 1: W, 2: E)
+        # - The Counter's key is the score difference, value is the number of ways to achieve it
+        dp = [[Counter() for j in range(3)] for i in range(n)]
+
+        for i in range(n):
+            for j in range(3): # Bob's current move
+                # Calculate score difference for this round
+                # j - s[i]: calculate the difference between Bob's and Alice's moves
+                # + 1: shift is added to ensure we always have a positive number before applying the module operation
+                # % 3: wraps the result around to keep it in the range 0-2
+                # -1: shifts the range to -1, 0, or 1
+                d = (j - s[i] + 1) % 3 - 1
+
+                if i == 0: # First round
+                    dp[i][j][d] = 1
+                else:
+                    for j2 in range(3):
+                        if j != j2:
+                            for v, count in dp[i - 1][j2].items():
+                                dp[i][j][v + d] = (dp[i][j][v + d] + count) % MOD
+        
+        ans = 0
+        for j in range(3):
+            for v in range(1, n + 1):
+                ans += dp[n - 1][j][v]
+        ans = ans % MOD
+
+        return ans
