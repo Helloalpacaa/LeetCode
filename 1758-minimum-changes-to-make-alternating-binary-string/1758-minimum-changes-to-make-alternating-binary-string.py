4cb15a3f906dad540e5a8bd44a47c2ee3f7ff4d3
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-11-05 13:27:48 -0500

    Time: 11 ms (38.78%), Space: 16.8 MB (14.63%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
@@ -3,6 +3,9 @@ class Solution:
         n = len(s)
         count = 0
         
+        # s[i]为0 or 1, 那么index i为偶数的时候，s[i]也为偶数，index为奇数的时候，s[i]也为奇数，这就组成了一个alternating string
+        # 以上条件要求最终的string为010101...计算它需要修正的characters数量为count
+        # 那么最终string为101010...需要修正的characters的数量就为n - count
         for i in range(n):
             if int(s[i]) != i % 2:
                 count += 1
