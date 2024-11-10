342f519c24ec6174b53bf2d39c5517c6c643c642
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-11-09 19:25:03 -0500

    Time: 562 ms (68.03%), Space: 20.7 MB (55.67%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -4,6 +4,9 @@ class Solution:
         ans = []
         
         for i in range(len(nums) - 2):
+            if nums[i] > 0:
+                break
+                
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
                 
