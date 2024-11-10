7dffb8d441933abd14101cf828a2070cc7e41965
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-11-10 17:21:18 -0500

    Time: 35 ms (15.66%), Space: 16.6 MB (88.63%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,25 +1,26 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        # 1. sort the candidates array to avoid duplicate combination
         candidates.sort()
-        ans = []
+        combinations = []
 
-        def backtracking(path: List[int], startIdx: int, total: int) -> None:
-            if total == target:
-                ans.append(path[:])
+        def backtracking(index: int, path: List[int], curr_sum) -> None:
+            if curr_sum == target:
+                combinations.append(path[:])
                 return
             
-            for i in range(startIdx, len(candidates)):
-                if total > target:
+            for i in range(index, len(candidates)):
+                if curr_sum > target:
                     break
                 
-                if i > startIdx and candidates[i] == candidates[i - 1]:
+                if i > index and candidates[i] == candidates[i - 1]:
                     continue
-                
+
                 path.append(candidates[i])
-                total += candidates[i]
-                backtracking(path, i + 1, total)
-                total -= candidates[i]
+                curr_sum += candidates[i]
+                backtracking(i + 1, path, curr_sum)
+                curr_sum -= candidates[i]
                 path.pop()
         
-        backtracking([], 0, 0)
-        return ans
+        backtracking(0, [], 0)
+        return combinations
