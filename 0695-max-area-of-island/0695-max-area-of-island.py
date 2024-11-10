fa515d11006129a944b617510cd51e3790bd9a6c
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-11-10 16:43:38 -0500

    Time: 17 ms (81.29%), Space: 17.3 MB (48.35%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,20 +1,23 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        max_area = 0
         m = len(grid)
         n = len(grid[0])
-
-        def dfs(i: int, j: int) -> int:
+        
+        def getArea(i, j) -> int:
+            # Check if the coordinate is out of boundry or is water
             if i < 0 or i >= m or j < 0 or j >= n or grid[i][j] == 0:
                 return 0
             
+            # Mark the island as visited
             grid[i][j] = 0
 
-            return 1 + dfs(i + 1, j) + dfs(i - 1, j) + dfs(i, j - 1) + dfs(i, j + 1)
+            return 1 + getArea(i + 1, j) + getArea(i - 1, j) + getArea(i, j + 1) + getArea(i, j - 1)
         
+        maxArea = 0
+
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 1:
-                    max_area = max(max_area, dfs(i, j))
+                    maxArea = max(maxArea, getArea(i, j))
         
-        return max_area
+        return maxArea
