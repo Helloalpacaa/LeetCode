fe7493a517de12d66ccf0ed49bf692cd89b14b58
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-11-11 14:55:58 -0500

    Time: 0 ms (100%), Space: 16.5 MB (59.09%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,10 +1,15 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        dp = [[0] * 2 for _ in range(n + 1)]
+        # dp[i][0]:rob nums[i], which means you cannot rob nums[i - 1]
+        # dp[i][1]: not rob nums[i], you can choose to rob nums[i - 1] or not
+        dp = [[0] * 2 for _ in range(n)]
+        dp[0][0] = nums[0]
+        dp[0][1] = 0
 
-        for i in range(n):
-            dp[i + 1][0] = dp[i][1] + nums[i] #抢
-            dp[i + 1][1] = max(dp[i][0], dp[i][1]) #不抢
+        for i in range(1, n):
+            dp[i][0] = nums[i] + dp[i - 1][1]
+            dp[i][1] = max(dp[i - 1][0], dp[i - 1][1])
         
-        return max(dp[n][0], dp[n][1])
+        return max(dp[n - 1][0], dp[n - 1][1])
+
