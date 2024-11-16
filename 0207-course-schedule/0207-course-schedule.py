47a44a68553efb0412581dfdec3f5dbee685ad3e
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-11-16 08:31:41 -0500

    Time: 4 ms (69.11%), Space: 17.9 MB (90.46%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,32 +1,50 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # courseList = [[] for _ in range(numCourses)]
+        # for course, pre in prerequisites:
+        #     courseList[course].append(pre)
+        
+        # exploring = set()
+        # visited = set()
+
+        # def dfs(course) -> bool:
+        #     if course in exploring:
+        #         return False
+            
+        #     if course in visited:
+        #         return True
+
+        #     exploring.add(course)
+
+        #     for pre in courseList[course]:
+        #         if not dfs(pre):
+        #             return False
+            
+        #     exploring.remove(course)
+        #     visited.add(course)
+
+        #     return True
+        
+        # for course, pre in prerequisites:
+        #     if not dfs(course):
+        #         return False
+        
+        # return True
+
+        indegree = [0] * numCourses
         courseList = [[] for _ in range(numCourses)]
         for course, pre in prerequisites:
-            courseList[course].append(pre)
+            indegree[course] += 1
+            courseList[pre].append(course)
         
-        exploring = set()
-        visited = set()
+        queue = deque([course for course in range(numCourses) if indegree[course] == 0])
 
-        def dfs(course) -> bool:
-            if course in exploring:
-                return False
-            
-            if course in visited:
-                return True
+        while queue:
+            pre = queue.popleft()
+            for course in courseList[pre]:
+                indegree[course] -= 1
+                if indegree[course] == 0:
+                    queue.append(course)
 
-            exploring.add(course)
+        return all(indegree[course] == 0 for course in range(numCourses))
 
-            for pre in courseList[course]:
-                if not dfs(pre):
-                    return False
-            
-            exploring.remove(course)
-            visited.add(course)
-
-            return True
-        
-        for course, pre in prerequisites:
-            if not dfs(course):
-                return False
-        
-        return True
