92e49250afb63267d8a586684d64fd7de98e1a15
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-11-16 07:10:49 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.py
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -2,19 +2,16 @@ class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
         father = [i for i in range(n)]
 
-        def join(u, v) -> None:
-            u = find(u)
-            v = find(v)
-            if u != v:
-                father[v] = u
-        
-        def find(u) -> int:
+        def find(u: int) -> int:
             if father[u] != u:
                 father[u] = find(father[u])
             return father[u]
         
-        if len(edges) < n - 1:
-            return False
+        def join(u: int, v: int) -> None:
+            u = find(u)
+            v = find(v)
+            if u != v:
+                father[v] = u
         
         for u, v in edges:
             if find(u) == find(v):
