a28c9d25a1d6825eae5dbccdeb597801f2a89ef5
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-11-18 10:13:36 -0500

    Time: 3 ms (81.7%), Space: 16.7 MB (19.67%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,30 +1,34 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        fresh = 0
-        queue = deque()
         m = len(grid)
         n = len(grid[0])
+        
+        queue = deque()
+        fresh = 0
 
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 1:
                     fresh += 1
                 elif grid[i][j] == 2:
-                    queue.append((i, j))
+                    queue.append([i, j])
 
-        minutes = 0
-        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        minute = 0
+        directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+
         while fresh > 0 and queue:
             size = len(queue)
-            for _ in range(size):
+            while size > 0:
                 i, j = queue.popleft()
                 for direction in directions:
                     next_i = i + direction[0]
                     next_j = j + direction[1]
                     if 0 <= next_i < m and 0 <= next_j < n and grid[next_i][next_j] == 1:
-                        queue.append((next_i, next_j))
-                        grid[next_i][next_j] = 2
                         fresh -= 1
-            minutes += 1
+                        queue.append([next_i, next_j])
+                        grid[next_i][next_j] = 2
+                size -= 1
+            minute += 1
         
-        return -1 if fresh > 0 else minutes
+        return minute if fresh == 0 else -1
+
