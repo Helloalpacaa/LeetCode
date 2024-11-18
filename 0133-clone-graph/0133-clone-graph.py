2e6cf91df68ea5d4ffad12b71a9e22277328a7cd
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-11-18 10:47:09 -0500

    Time: 44 ms (28.03%), Space: 17 MB (27.19%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -9,21 +9,21 @@ class Node:
 from typing import Optional
 class Solution:
     def __init__(self):
-        # 建立一个hashmap存储value: node，key: clone
-        # 把1 clone以后去clone它的neighbor 2, 2的neighbor里又会包含1，又回到了1，这时候直接return clone的1不需要再去clone 1以及它的neighbor
-        self.visited = {}
+        self.graph = {}
 
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         if node is None:
             return None
         
-        if node in self.visited:
-            return self.visited[node]
+        if node in self.graph:
+            return self.graph[node]
 
-        clone = Node(node.val, [])
-        self.visited[node] = clone
+        
+        clone = Node(node.val)
+        self.graph[node] = clone
 
         for neighbor in node.neighbors:
             clone.neighbors.append(self.cloneGraph(neighbor))
         
         return clone
+
