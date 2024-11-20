f053e9f52cce6b9c6d52632e8a8df12b4409f8e1
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-11-20 15:45:35 -0500

    Time: 15 ms (92.2%), Space: 17.7 MB (29.58%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
@@ -1,19 +1,14 @@
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
-        m = len(matrix)
-        n = len(matrix[0])
-        dp = [[0] * n for _ in range(m)]
+        n = len(matrix)
 
-        for col in range(n):
-            dp[0][col] = matrix[0][col]
-        
-        for i in range(1, m):
+        for i in range(1, n):
             for j in range(n):
                 if j == 0:
-                    dp[i][j] = min(dp[i - 1][j], dp[i - 1][j + 1]) + matrix[i][j]
+                    matrix[i][j] = min(matrix[i - 1][j], matrix[i - 1][j + 1]) + matrix[i][j]
                 elif j == n - 1:
-                    dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j]) + matrix[i][j]
+                    matrix[i][j] = min(matrix[i - 1][j], matrix[i - 1][j - 1]) + matrix[i][j]
                 else:
-                    dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j], dp[i - 1][j + 1]) + matrix[i][j]
+                    matrix[i][j] = min(matrix[i - 1][j - 1], matrix[i - 1][j], matrix[i - 1][j + 1]) + matrix[i][j]
         
-        return min(dp[m - 1])
+        return min(matrix[n - 1][col] for col in range(n))
