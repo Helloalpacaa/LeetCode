eba09e89f5ff50b4b534902e655c861570b123b1
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-11-20 15:33:27 -0500

    Time: 319 ms (89.63%), Space: 18.9 MB (35.08%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -4,10 +4,10 @@ class Solution:
         # dp[i][j]: minimum number os steps to make s[i: j] palindrome
         dp = [[0] * n for _ in range(n)]
 
-        for i in range(n - 1, -1, -1):
-            for j in range(i + 1, n):
+        for i in range(n - 2, -1, -1):
+            for j in range(i + 1, n): # i == j时，dp[i][j] == 0,可以直接跳过
                 if s[i] == s[j]:
-                    dp[i][j] = dp[i + 1][j - 1]
+                    dp[i][j] = dp[i + 1][j - 1] # 可以直接用dp[i + 1][j - 1]因为 i != j，所以i从n - 2开始，j从n - 1开始
                 else:
                     dp[i][j] = min(dp[i + 1][j], dp[i][j - 1]) + 1
         
