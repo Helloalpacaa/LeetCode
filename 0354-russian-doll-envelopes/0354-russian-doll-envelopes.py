2b0b41ce986f5e1353df9b3d97aa2ca77fc5caa3
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-11-20 18:18:50 -0500

    Time: 945 ms (5.15%), Space: 64.2 MB (44.67%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.py
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.py
@@ -2,13 +2,20 @@ class Solution:
     def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
         envelopes.sort(key=lambda x: (x[0], -x[1]))
         n = len(envelopes)
-        dp = [1] * n
 
-        print(envelopes)
+        heights = [env[1] for env in envelopes]
+        # dp[i]: 长度为i + 1的LIS的最小end，the smalledst height that ends an increasing subsequence of length i + 1
+        dp = []
 
-        for i in range(1, n):
-            for j in range(0, i):
-                if envelopes[j][1] < envelopes[i][1]:
-                    dp[i] = max(dp[i], dp[j] + 1)
+        for height in heights:
+            # 找到height在dp里的位置
+            idx = bisect_left(dp, height)
+            if idx == len(dp): # 说明height比所有dp里的值都大
+                dp.append(height)
+            else:
+                dp[idx] = height
+            # 假设现在dp是[3,4]，遇到2，idx为0，说明2肯定比当前在index为0上的数字小，那么dp变为[2,4]
+            # 长度为1的递增数组的最小height为2，这样是为了保留持续递增的可能性
         
-        return max(dp)
+        return len(dp)
+        
