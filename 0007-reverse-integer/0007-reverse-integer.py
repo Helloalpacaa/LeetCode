5b805881fd163012db86473aaa5840ca1459383a
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-11-27 08:57:35 -0500

    Time: 33 ms (74.89%), Space: 17.4 MB (6.37%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -26,7 +26,7 @@ class Solution:
             digit = x % 10
             x = x // 10
 
-            if result > (2**31 - 1) // 10 or result > 2**31 // 10:
+            if result > (2**31 - 1) // 10:
                 return 0
 
             result = result * 10 + digit
