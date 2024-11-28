aa1aecd316e150874f497f84f798f833dfbd18de
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-11-28 09:41:52 -0500

    Time: 42 ms (17.11%), Space: 17.4 MB (6.57%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,35 +1,13 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        # negative = x < 0
-        # x = abs(x)
-
-        # Converting an integer to string takes O(log x) time
-        # String reversal using slice takes O(log x) time as we process each digit
-        # Converting back to integer takes O(log x)
-        # Space Complexity: O(log x), we need to store the string representation of the number
-        # reversed_number = int(str(x)[::-1])
-
-        # result = -reversed_number if negative else reversed_number
-
-        # if result < -2**31 or result > 2**31 - 1:
-        #     return 0
-        
-        # return result
-
         sign = 1
         if x < 0:
             sign = -1
         
-        result = 0
         x = abs(x)
-        while x > 0:
-            digit = x % 10
-            x = x // 10
+        reversed_number = int(str(x)[::-1])
 
-            if result > (2**31 - 1) // 10:
-                return 0
-
-            result = result * 10 + digit
-
-        return result * sign
+        if reversed_number < -2**31 or reversed_number > 2**31 - 1:
+            return 0
         
+        return reversed_number * sign
