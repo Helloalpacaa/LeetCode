88da1fa2f0017599345f577c1423681b3b0452f2
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-11-29 22:01:56 -0500

    Time: 51 ms (38.78%), Space: 29.3 MB (32.86%) - LeetHub

---
+++ 2892-minimizing-array-after-replacing-pairs-with-their-product/2892-minimizing-array-after-replacing-pairs-with-their-product.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minArrayLength(self, nums: List[int], k: int) -> int:
+        stack = []
+
+        for num in nums:
+            while stack and stack[-1] * num <= k:
+                num = stack.pop() * num
+            stack.append(num)
+        
+        return len(stack)
