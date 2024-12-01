4606087a32d69d341076eb5c037c42e0d78d1317
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-12-01 10:53:58 -0500

    Time: 833 ms (39.83%), Space: 17.6 MB (35.22%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,17 +1,11 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        # dp[i]: 容量为i的背包，最少可以用几个硬币装满
-        dp = [-1] * (amount + 1)
+        dp = [float('inf')] * (amount + 1)
         dp[0] = 0
 
         for coin in coins:
-            for i in range(coin, amount + 1):
-                if dp[i - coin] == -1:
-                    continue
-                elif dp[i] == -1:
-                    dp[i] = dp[i - coin] + 1
-                else:
+            for i in range(amount + 1):
+                if i >= coin:
                     dp[i] = min(dp[i], dp[i - coin] + 1)
         
-        return dp[amount]
-                    
+        return dp[amount] if dp[amount] != float('inf') else -1
