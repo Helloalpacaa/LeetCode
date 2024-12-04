36ec86b5c534fee8111a868d266ba1a22d98fd13
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-12-04 10:50:48 -0500

    Time: 10 ms (30.79%), Space: 18.7 MB (5.49%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,43 +1,30 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        # Union Find
-        # n = len(isConnected)
-        # father = [i for i in range(n)]
-
-        # def join(u, v) -> None:
-        #     u = find(u)
-        #     v = find(v)
-        #     father[v] = u
-
-        # def find(u) -> int:
-        #     if father[u] != u:
-        #         father[u] = find(father[u])
-        #     return father[u]
-
-        # provinces = n
-        # for i in range(n):
-        #     for j in range(n):
-        #         if isConnected[i][j] == 1 and find(i) != find(j):
-        #             provinces -= 1
-        #             join(i, j)
-        
-        # return provinces
-
-        # DFS
-        visited = set()
+        # 1. define a list with each element has its own as their father
         n = len(isConnected)
+        father = [i for i in range(n)]
 
-        def dfs(city) -> None:
-            visited.add(city)
-
-            for neighbor in range(n):
-                if isConnected[city][neighbor] == 1 and neighbor not in visited:
-                    dfs(neighbor)
+        # union find
+        # join two proinces if they don't have the same father
+        def join(u, v) -> None:
+            u = find(u)
+            v = find(v)
+            if u != v:
+                father[v] = u
         
-        provinces = 0
-        for city in range(n):
-            if city not in visited:
-                provinces += 1
-                dfs(city)
+        # find and return the father of u, also using path compress
+        def find(u) -> int:
+            if u != father[u]:
+                father[u] = find(father[u])
+            return father[u]
+        
+        provinces = n
+        for i in range(n):
+            for j in range(n):
+                if i != j and isConnected[i][j] == 1:
+                    if find(i) != find(j):
+                        join(i, j)
+                        provinces -= 1
         
         return provinces
+        
