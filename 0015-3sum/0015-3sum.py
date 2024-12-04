6bf87fa568b631d4fde09a8ae954967f2b43a628
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-12-04 10:11:24 -0500

    Time: 459 ms (86.81%), Space: 20.2 MB (37.05%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,31 +1,43 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        ans = []
-        n = len(nums)
+        # 1. sort nums
         nums.sort()
+        
+        ans = []
 
-        for i in range(n):
+        # 2. iterate each element in nums, using two pointers to look for the solution
+        for i in range(len(nums)):
+            # skip the duplicate value
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            
+
+            # if nums[i] > 0, there is no chance that we can find a solution that 3sum == 0
             if nums[i] > 0:
                 break
             
             left = i + 1
-            right = n - 1
+            right = len(nums) - 1
+
             while left < right:
-                total = nums[i] + nums[left] + nums[right]
-                if total == 0:
+                three_sum = nums[i] + nums[left] + nums[right]
+
+                # if 3sum == 0, add it into ans, increment left pointer and decrement right pointer until left == right
+                if three_sum == 0:
                     ans.append([nums[i], nums[left], nums[right]])
-                    while left < right and nums[left + 1] == nums[left]:
-                        left += 1
-                    while left < right and nums[right - 1] == nums[right]:
-                        right -= 1
                     left += 1
                     right -= 1
-                elif total < 0:
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+                    
+                # if 3sum < 0, we increment the left poiner
+                elif three_sum < 0:
                     left += 1
+                # if 3sum > 0, we decrement the right pointer
                 else:
                     right -= 1
         
         return ans
+                
+                
