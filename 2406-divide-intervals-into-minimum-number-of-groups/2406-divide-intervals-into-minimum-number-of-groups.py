ccb931296aa885d8c8e29c5a248bcff96dd86ed2
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-12-04 14:08:13 -0500

    Time: 87 ms (99.61%), Space: 46.3 MB (95.55%) - LeetHub

--- 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.py
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.py
@@ -1,7 +1,9 @@
 class Solution:
     def minGroups(self, intervals: List[List[int]]) -> int:
-        # 1, 1, 2, 5, 6
-        # 3, 5, 8, 10, 10
+        # [1, 1, 2, 5, 6]
+        # [3, 5, 8, 10, 10]
+
+        # find each interval's start and end time, then sort them
         start = []
         end = []
         for interval in intervals:
@@ -11,6 +13,8 @@ class Solution:
         start.sort()
         end.sort()
 
+        # use two pointers to track the current start time and end time
+        # if start time < end time, there is overlapping and we need to increment the group numbers
         j = 0
         groups = 0
         for i in range(len(start)):
