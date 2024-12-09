b54c77df600f033bb6c15ce1166623ccb4bf08bc
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-12-08 21:04:28 -0500

    Time: 15 ms (49.13%), Space: 20.4 MB (41.38%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,16 +1,15 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # 1. created a hashmap with key(sorted string): value(List[str])
-        hashmap = dict()
-
-        for s in strs:
-            sorted_s = ''.join(sorted(s))
-            if sorted_s not in hashmap:
-                hashmap[sorted_s] = []
-            hashmap[sorted_s].append(s)
+        groups = {}
+        
+        for word in strs:
+            sorted_word = "".join(sorted(word))
+            if sorted_word not in groups:
+                groups[sorted_word] = []
+            groups[sorted_word].append(word)
         
         ans = []
-        for key, value in hashmap.items():
-            ans.append(value)
+        for values in groups.values():
+            ans.append(values)
         
         return ans
