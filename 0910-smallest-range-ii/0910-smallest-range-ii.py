9a9da766409143156af4fbb5a27af09d781da2eb
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-12-09 00:24:17 -0500

    Time: 32 ms (45.95%), Space: 18.3 MB (5.6%) - LeetHub

--- 0910-smallest-range-ii/0910-smallest-range-ii.py
+++ 0910-smallest-range-ii/0910-smallest-range-ii.py
@@ -1,14 +1,13 @@
 class Solution:
     def smallestRangeII(self, nums: List[int], k: int) -> int:
-        # [1, 10, 6, 9], k = 2
-        # [3, 8, 4, 7]
-        # [1, 6, 9, 10]
+        if len(nums) <= 1:
+            return 0
         nums.sort()
-        min_score = nums[-1] - nums[0]
+        score = nums[-1] - nums[0]
 
         for i in range(len(nums) - 1):
-            local_min = min(nums[0] + k, nums[i + 1] - k)
-            local_max = max(nums[-1] - k, nums[i] + k)
-            min_score = min(min_score, local_max - local_min)
+            max_number = max(nums[-1] - k, nums[i] + k)
+            min_number = min(nums[0] + k, nums[i + 1] - k)
+            score = min(score, max_number - min_number)
         
-        return min_score
+        return score
