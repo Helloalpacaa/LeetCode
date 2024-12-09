70aadf57052c20c7cb9c7c26ee2a4d89985f2e2e
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-12-09 00:44:37 -0500

    Time: 79 ms (54.93%), Space: 31.1 MB (9.61%) - LeetHub

--- 2422-merge-operations-to-turn-array-into-a-palindrome/2422-merge-operations-to-turn-array-into-a-palindrome.py
+++ 2422-merge-operations-to-turn-array-into-a-palindrome/2422-merge-operations-to-turn-array-into-a-palindrome.py
@@ -2,18 +2,19 @@ class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
         left = 0
         right = len(nums) - 1
-        count = 0
-        while left < right:
+        operations = 0
+
+        while left <= right:
             if nums[left] == nums[right]:
                 left += 1
                 right -= 1
             elif nums[left] < nums[right]:
+                nums[left + 1] = nums[left + 1] + nums[left]
                 left += 1
-                nums[left] += nums[left - 1]
-                count += 1
+                operations += 1
             else:
+                nums[right - 1] = nums[right - 1] + nums[right]
                 right -= 1
-                nums[right] += nums[right + 1]
-                count += 1
+                operations += 1
         
-        return count
+        return operations
