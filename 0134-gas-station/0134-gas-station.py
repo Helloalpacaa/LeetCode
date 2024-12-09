21908bf823aaf0fc30834b1422d490288ab0865a
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-12-08 22:46:41 -0500

    Time: 915 ms (5.08%), Space: 22 MB (99.5%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,5 +1,8 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        # 如果从start point出发，到i时油不够了，那么不管从start point 到i之间的哪个point出发都是不行的
+        # 所以我们重置start point到i + 1
+        # 因为同时在tracking total，如果遍历完总体的gas - cost < 0的话说明没有valid的start point
         subtotal, total, start = 0, 0, 0
         for i in range(len(gas)):
             subtotal += gas[i] - cost[i]
