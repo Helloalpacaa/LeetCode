e269edf7d23ba807b376b3cfa335f05721c6ac6a
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-12-08 23:10:28 -0500

    Time: 3 ms (85.45%), Space: 19.8 MB (26.84%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,10 +1,8 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        if len(intervals) == 0:
-            return 0
-        
         start = []
         end = []
+
         for interval in intervals:
             start.append(interval[0])
             end.append(interval[1])
@@ -14,13 +12,10 @@ class Solution:
 
         rooms = 0
         j = 0
-        for i in range(len(end)):
+        for i in range(len(start)):
             if start[i] < end[j]:
                 rooms += 1
             else:
                 j += 1
         
         return rooms
-
-        
-        return count
