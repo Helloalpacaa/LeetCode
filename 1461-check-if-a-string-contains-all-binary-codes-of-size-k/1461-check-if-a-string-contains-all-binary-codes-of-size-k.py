6eac6b44429ff0a4bd9dc7bbc419ed02750265ec
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2024-12-12 07:48:59 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
@@ -2,15 +2,8 @@ class Solution:
     def hasAllCodes(self, s: str, k: int) -> bool:
         # 把每个size为k的不同的substring加进一个set里，最终检查set的数量
         substrings = set()
-        substring = ""
 
-        for i in range(len(s)):
-            substring += s[i]
-
-            if i >= k:
-                substring = s[i - k + 1: i + 1]
-            
-            if len(substring) == k:
-                substrings.add(substring)
+        for i in range(0, len(s) - k):
+            substrings.add(s[i: i + k])
         
         return len(substrings) == 2 ** k
