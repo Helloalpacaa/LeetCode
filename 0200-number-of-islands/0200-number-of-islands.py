a9042381d61ba23c2a0ecf4ce3467fe784959602
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-01-05 10:02:44 -0500

    Time: 227 ms (89.41%), Space: 20.4 MB (40.82%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,29 +1,24 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        islands = 0
         m = len(grid)
         n = len(grid[0])
-        
-        # flood the adjacent islands
-        def dfs(i, j) -> None:
-            # check edge
+
+        def dfs(i: int, j: int) -> None:
             if i < 0 or i >= m or j < 0 or j >= n or grid[i][j] == '0':
                 return
             
-            # flood the island by changing its value to '0'
             grid[i][j] = '0'
 
-            # flood its adjencent islands recursively
             dfs(i + 1, j)
             dfs(i - 1, j)
             dfs(i, j + 1)
             dfs(i, j - 1)
-        
-        # Iterating the grid, if we see an island, flood its adjencent lands
-        count = 0
+
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == '1':
-                    count += 1
+                    islands += 1
                     dfs(i, j)
         
-        return count
+        return islands
