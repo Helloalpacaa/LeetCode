4b5faf4bfe80670c41711444184c08314126604e
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-01-13 23:12:50 -0500

    Time: 11 ms (74.47%), Space: 19.6 MB (5.95%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,19 +1,15 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        # Descending monotonic stack, storing the index of height
-        stack = deque()
-        n = len(height)
+        # 单调递减栈，一旦遇到更大的值，就是接雨水的时候
+        monotonic_stack = deque()
         area = 0
 
-        for i in range(n):
-            while stack and height[i] > height[stack[-1]]:
-                base = height[stack.pop()]
-                if stack:
-                    left = stack[-1]
-                    width = i - left - 1
-                    area += width * (min(height[left], height[i]) - base)
-            stack.append(i)
+        for i in range(len(height)):
+            while monotonic_stack and height[i] > height[monotonic_stack[-1]]:
+                base = height[monotonic_stack.pop()]
+                if monotonic_stack:
+                    left = monotonic_stack[-1]
+                    area += (i - left - 1) * (min(height[left], height[i]) -  base)
+            monotonic_stack.append(i)
         
         return area
-            
-
