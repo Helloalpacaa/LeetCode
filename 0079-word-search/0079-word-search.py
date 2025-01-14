a1a47ed2360857caa3a00fafbdb401d83b6fd4f0
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-01-14 16:16:38 -0500

    Time: 4559 ms (42.87%), Space: 18 MB (18.7%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -3,29 +3,30 @@ class Solution:
         m = len(board)
         n = len(board[0])
         
-        def backtracking(i, j, k, visited) -> bool:
+        def backtracking(i, j, k) -> bool:
             if k == len(word):
                 return True
 
-            if i < 0 or i >= m or j < 0 or j >= n or (i, j) in visited or board[i][j] != word[k]:
+            if i < 0 or i >= m or j < 0 or j >= n or board[i][j] != word[k]:
                 return False
             
-            visited.add((i, j))
+            tmp = board[i][j]
+            board[i][j] = '#'
             
             for direction in [[-1, 0], [1, 0], [0, -1], [0, 1]]:
                 next_i = i + direction[0]
                 next_j = j + direction[1]
-                if backtracking(next_i, next_j, k + 1, visited):
+                if backtracking(next_i, next_j, k + 1):
                     return True
             
-            visited.remove((i, j))
+            board[i][j] = tmp
 
             return False
 
         for i in range(m):
             for j in range(n):
                 if board[i][j] == word[0]:
-                    if backtracking(i, j, 0, set()):
+                    if backtracking(i, j, 0):
                         return True
         
         return False
