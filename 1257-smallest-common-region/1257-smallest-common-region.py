d212878bfda940f1a5e10ee0a31f859268769025
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-01-18 09:55:34 -0500

    Time: 224 ms (33.91%), Space: 22.4 MB (28.99%) - LeetHub

--- 1257-smallest-common-region/1257-smallest-common-region.py
+++ 1257-smallest-common-region/1257-smallest-common-region.py
@@ -4,16 +4,14 @@ class Solution:
         for region in regions:
             for i in range(1, len(region)):
                 parent[region[i]] = region[0]
-        print(parent)
+
         ancestors1 = set()
 
         while region1:
             ancestors1.add(region1)
             region1 = parent.get(region1)
         
-        print(ancestors1)
         while region2 not in ancestors1:
-            print(region2)
             region2 = parent[region2]
         
         return region2
