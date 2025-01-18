655a7d451718c245229f78e7f9991496774d02f5
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-01-18 11:20:00 -0500

    Time: 5 ms (77.52%), Space: 24 MB (11.81%) - LeetHub

--- 0251-flatten-2d-vector/0251-flatten-2d-vector.py
+++ 0251-flatten-2d-vector/0251-flatten-2d-vector.py
@@ -1,20 +1,19 @@
 class Vector2D:
 
     def __init__(self, vec: List[List[int]]):
-        self.vec = []
+        self.flatten = []
         for vector in vec:
             for num in vector:
-                self.vec.append(num)
+                self.flatten.append(num)
         self.pointer = 0
 
     def next(self) -> int:
-        
-        ans = self.vec[self.pointer]
+        ans = self.flatten[self.pointer]
         self.pointer += 1
         return ans
 
     def hasNext(self) -> bool:
-        return self.pointer < (len(self.vec))
+        return self.pointer < (len(self.flatten))
 
 
 # Your Vector2D object will be instantiated and called as such:
