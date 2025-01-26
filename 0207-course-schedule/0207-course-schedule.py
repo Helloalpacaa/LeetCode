f749211bc6df08b6434fe9548b03c1b3d7614193
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-01-25 22:55:03 -0500

    Time: 1 ms (93.32%), Space: 19 MB (66.06%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,43 +1,12 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # courseList = [[] for _ in range(numCourses)]
-        # for course, pre in prerequisites:
-        #     courseList[course].append(pre)
-        
-        # exploring = set()
-        # visited = set()
-
-        # def dfs(course) -> bool:
-        #     if course in exploring:
-        #         return False
-            
-        #     if course in visited:
-        #         return True
-
-        #     exploring.add(course)
-
-        #     for pre in courseList[course]:
-        #         if not dfs(pre):
-        #             return False
-            
-        #     exploring.remove(course)
-        #     visited.add(course)
-
-        #     return True
-        
-        # for course, pre in prerequisites:
-        #     if not dfs(course):
-        #         return False
-        
-        # return True
-
         indegree = [0] * numCourses
         courseList = [[] for _ in range(numCourses)]
         for course, pre in prerequisites:
             indegree[course] += 1
             courseList[pre].append(course)
         
-        queue = deque([course for course in range(numCourses) if indegree[course] == 0])
+        queue = deque([c for c in range(numCourses) if indegree[c] == 0])
 
         while queue:
             pre = queue.popleft()
@@ -45,6 +14,5 @@ class Solution:
                 indegree[course] -= 1
                 if indegree[course] == 0:
                     queue.append(course)
-
+        
         return all(indegree[course] == 0 for course in range(numCourses))
-
