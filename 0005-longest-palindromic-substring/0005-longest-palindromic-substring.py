de9c9360f4b04ec6d8f2d760b961d6eb6268c3d6
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-01-28 17:39:49 -0500

    Time: 201 ms (94.73%), Space: 17.9 MB (50.63%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,18 +1,25 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        def palindrome(i: int, j: int) -> str:
-            while i >= 0 and j < len(s) and s[i] == s[j]:
-                i -= 1
-                j += 1
+        n = len(s)
 
-            return s[i + 1: j]
+        def palindrome(left, right) -> str:
+            while left >= 0 and right < n and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left + 1 : right]
         
-        ans = ""
-        for i in range(len(s)):
-            s1 = palindrome(i, i)
-            s2 = palindrome(i, i + 1)
-            ans = s1 if len(s1) > len(ans) else ans
-            ans = s2 if len(s2) > len(ans) else ans
-        
-        return ans
+        longest_palindrome = ""
+        longest = 0
+        for i in range(n):
+            valid_palindrome = palindrome(i, i)
             
+            if len(valid_palindrome) > longest:
+                longest_palindrome = valid_palindrome
+                longest = len(valid_palindrome)
+            if i + 1 < n and s[i] == s[i + 1]:
+                valid_palindrome = palindrome(i, i + 1)
+                if len(valid_palindrome) > longest:
+                    longest_palindrome = valid_palindrome
+                    longest = len(valid_palindrome)
+
+        return longest_palindrome
