5a10a66ead161ce4260e9db0bcbb40e306827551
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-01-27 22:46:10 -0500

    Time: 137 ms (60.95%), Space: 78.6 MB (24.06%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,65 +1,62 @@
 class Node:
+
     def __init__(self, key: int, val: int):
-        self.key = key
-        self.val = val
         self.prev = None
         self.next = None
+        self.key = key
+        self.val = val
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.capacity = capacity
         self.cache = {}
-
-        # build a doubly linkedlist to store the LRU at the tail, every Node stores (key, value)
+        self.capacity = capacity
         self.head = Node(0, 0)
         self.tail = Node(0, 0)
         self.head.next = self.tail
         self.tail.prev = self.head
-
+        
 
     def get(self, key: int) -> int:
         if key not in self.cache:
             return -1
         
-        # we need to move this key to the head of the Linkedlist
         node = self.cache[key]
+        # 把这个node从原本在linkedlist里的位置移到head后面
         self.remove(node)
         self.add(node)
 
         return node.val
-  
+        
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             node = self.cache[key]
+            node.val = value
             self.remove(node)
-        
-        node = Node(key, value)
-        self.cache[key] = node
-        self.add(node)
+            self.add(node)
+        else:
+            node = Node(key, value)
+            self.add(node)
+            self.cache[key] = node
+            if len(self.cache) > self.capacity:
+                LRU = self.tail.prev
+                self.remove(LRU)
+                del self.cache[LRU.key]
 
-        if len(self.cache) > self.capacity:
-            LRU = self.tail.prev
-            self.remove(LRU)
-            del self.cache[LRU.key]
 
+    def remove(self, node: Node) -> None:
+        next_node = node.next
+        prev = node.prev
+        prev.next = next_node
+        next_node.prev = prev
     
-    # add new node right after head
     def add(self, node: Node) -> None:
         next_node = self.head.next
         self.head.next = node
         node.prev = self.head
         node.next = next_node
         next_node.prev = node
-        
-    
-    # remove the LRU before the tail
-    def remove(self, node) -> None:
-        prev = node.prev
-        prev.next = node.next
-        node.next.prev = prev
-
         
 
 
