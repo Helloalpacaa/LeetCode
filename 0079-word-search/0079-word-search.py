ea1dfb0be3b6ef71e20cbba21a3458c7e82bfafb
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-01-31 11:08:04 -0500

    Time: 4484 ms (48.14%), Space: 17.8 MB (49.2%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -2,29 +2,31 @@ class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         m = len(board)
         n = len(board[0])
-        directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
         
-        def backtracking(i, j, k, visited) -> bool:
+        def backtracking(i, j, k) -> bool:
             if k == len(word):
                 return True
 
-            if i < 0 or i >= m or j < 0 or j >= n or board[i][j] != word[k] or (i, j) in visited:
+            if i < 0 or i >= m or j < 0 or j >= n or board[i][j] != word[k]:
                 return False
             
-            visited.add((i, j))
+            tmp = board[i][j]
+            board[i][j] = '#' # change the value is faster then using a set to track the visited grid
             
-            for direction in directions:
+            for direction in [[-1, 0], [1, 0], [0, -1], [0, 1]]:
                 next_i = i + direction[0]
                 next_j = j + direction[1]
-                if backtracking(next_i, next_j, k + 1, visited):
+                if backtracking(next_i, next_j, k + 1):
                     return True
             
-            visited.remove((i, j))
+            board[i][j] = tmp
+
             return False
-        
+
         for i in range(m):
             for j in range(n):
-                if board[i][j] == word[0] and backtracking(i, j, 0, set()):
-                    return True
+                if board[i][j] == word[0]:
+                    if backtracking(i, j, 0):
+                        return True
         
         return False
