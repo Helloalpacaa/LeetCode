e352e8680e8afcb70716d8f7b7b5acfb20acd2ed
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-02-01 23:31:55 -0500

    Time: 18 ms (5.54%), Space: 21.7 MB (5.55%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,22 +1,14 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
-        # dp[i][0]: 第i天持有股票的最大profit
-        # dp[i][1]: 第i天不持有股票的最大profit
-        # dp = [[0] * 2 for _ in range(n)]
-        # dp[0][0] = -prices[0]
-
-        # for i in range(1, n):
-        #     dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] - prices[i])
-        #     dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i])
+        dp = [[0] * 2 for _ in range(n)]
+        # 0: max profit of buy in day 1
+        # 1: max profit of sell in day 1
+        dp[0][0] = -prices[0]
         
-        # return dp[n - 1][1]
-
-        hold = -prices[0]
-        not_hold = 0
-
         for i in range(1, n):
-            hold = max(hold, not_hold - prices[i])
-            not_hold = max(not_hold, hold + prices[i])
-
-        return not_hold
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] - prices[i])
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i])
+        
+        print(dp)
+        return dp[n - 1][1]
