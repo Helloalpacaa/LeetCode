4fdc10e5d68f7dc08b5a3ddbd0c5ec49d974d0ab
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-02-02 15:29:55 -0500

    Time: 93 ms (99.74%), Space: 28.1 MB (94.67%) - LeetHub

--- 3371-identify-the-largest-outlier-in-an-array/3371-identify-the-largest-outlier-in-an-array.py
+++ 3371-identify-the-largest-outlier-in-an-array/3371-identify-the-largest-outlier-in-an-array.py
@@ -1,21 +1,16 @@
 class Solution:
     def getLargestOutlier(self, nums: List[int]) -> int:
-        total = sum(nums)
+        # special_number * 2 + outlier = sum
+        # outlier = sum - special_number * 2
+
         count = Counter(nums)
-        largest_outlier = float('-inf')
+        max_outlier = float('-inf')
+        total = sum(nums)
 
-        # for num in nums:
-        #     sum_element = (total - num) // 2 # 会过不了所有的test case，因为假设[7, 8, 10],10会被当成outlier
-        #     if sum_element in count:
-        #         if num != sum_element or count[sum_element] > 1:
-        #             largest_outlier = max(largest_outlier, num)
-
-        # 我们不能iterate number来assume它是potential outlier，而是assume它是sum_element然后去找outlier
         for num in count:
             potential_outlier = total - num * 2
-            
-            if potential_outlier in count:
-                if potential_outlier != num or count[num] > 1:
-                    largest_outlier = max(largest_outlier, potential_outlier)
 
-        return largest_outlier
+            if (potential_outlier in count) and (potential_outlier != num or count[potential_outlier] >= 2):
+                max_outlier = max(max_outlier, potential_outlier)
+        
+        return max_outlier
