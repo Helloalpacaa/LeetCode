f89132f6dd330777ac21e018059dba282026593f
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-02-01 23:44:21 -0500

    Time: 0 ms (100%), Space: 17.8 MB (26.95%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,11 +1,17 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        dp = [[0] * 2 for _ in range(n)]
-        dp[0][0] = nums[0] # 0 = rob, 1 = not_rob
+        # dp = [[0] * 2 for _ in range(n)]
+        # dp[0][0] = nums[0] # 0 = rob, 1 = not_rob
+        rob = nums[0]
+        not_rob = 0
 
         for i in range(1, len(nums)):
-            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + nums[i])
-            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0])
+            # dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + nums[i])
+            # dp[i][1] = max(dp[i - 1][1], dp[i - 1][0])
+            tmp = rob
+            rob = max(rob, not_rob + nums[i])
+            not_rob = max(not_rob, tmp)
         
-        return max(dp[n - 1][0], dp[n - 1][1])
+        # return max(dp[n - 1][0], dp[n - 1][1])
+        return max(rob, not_rob)
