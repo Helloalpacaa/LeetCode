5cb5480ba865f61233bf21804ae4a6f0b725afa3
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-02-01 23:33:27 -0500

    Time: 7 ms (10.21%), Space: 21.2 MB (5.55%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -10,5 +10,4 @@ class Solution:
             dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] - prices[i])
             dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i])
         
-        print(dp)
         return dp[n - 1][1]
