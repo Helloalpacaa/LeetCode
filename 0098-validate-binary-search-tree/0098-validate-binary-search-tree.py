cceb3c2790757f7cd810e5cd8c463abb466d8fbb
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-02-14 00:10:41 -0500

    Time: 2 ms (41.04%), Space: 19.8 MB (65.93%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -5,20 +5,42 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        stack = []
-        prev = float('-inf')
+    
 
-        while root or stack:
-            while root:
-                stack.append(root)
-                root = root.left
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        # stack = []
+        # prev = float('-inf')
+
+        # while root or stack:
+        #     while root:
+        #         stack.append(root)
+        #         root = root.left
             
-            root = stack.pop()
-            if root.val <= prev:
-                return False
-            prev = root.val
-            root = root.right
+        #     root = stack.pop()
+        #     if root.val <= prev:
+        #         return False
+        #     prev = root.val
+        #     root = root.right
         
-        return True
+        # return True
+        self.prev = None
+        
+        def traverse(node: Optional[TreeNode]) -> bool:
+            if node is None:
+                return True
+            
+            left = traverse(node.left)
+
+            if self.prev is not None and node.val <= self.prev:
+                return False
+            
+            self.prev = node.val
+            right = traverse(node.right)
+
+            return left and right
+        
+        return traverse(root)
+
+        
+
         
