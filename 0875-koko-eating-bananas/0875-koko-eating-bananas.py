fff205d860e15311bbbef870b11b586636314402
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-02-16 18:01:09 -0500

    Time: 146 ms (86.13%), Space: 19 MB (49.2%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -3,15 +3,16 @@ class Solution:
         left = 1
         right = max(piles)
 
-        while left <= right:
+        while left < right:
             mid = (left + right) // 2
             hours = 0
             for pile in piles:
                 hours += (pile + mid - 1) // mid
             
             if hours <= h:
-                right = mid - 1
+                right = mid
             else:
                 left = mid + 1
+            print(left, right, mid)
         
         return left
