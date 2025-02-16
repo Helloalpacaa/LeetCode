9995e11b8431ca3396517c191ba06ba7a715b4ca
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-02-16 10:03:23 -0500

    Time: 146 ms (36.36%), Space: 48 MB (93.33%) - LeetHub

---
+++ 2055-plates-between-candles/2055-plates-between-candles.py
@@ -0,0 +1,22 @@
+class Solution:
+    def platesBetweenCandles(self, s: str, queries: List[List[int]]) -> List[int]:
+        arr = []
+        for i in range(len(s)):
+            if s[i] == '|':
+                arr.append(i)
+        
+        prefix_sum = [0] * len(arr)
+        for i in range(1, len(arr)):
+            prefix_sum[i] = prefix_sum[i - 1] + (arr[i] - arr[i - 1] - 1)
+        
+        ans = []
+        for query in queries:
+            left = bisect.bisect_left(arr, query[0])
+            right = bisect.bisect_right(arr, query[1]) - 1
+            # print(left, right, arr)
+            if left >right or left >= len(arr) or right < 0:
+                ans.append(0)
+            else:
+                ans.append(prefix_sum[right] - prefix_sum[left])
+        
+        return ans
