194ae988010dd709a1aace65527e7e3f319a8c4f
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-02-17 18:45:39 -0500

    Time: 1 ms (33.51%), Space: 20.2 MB (19.88%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.swift
+++ 0020-valid-parentheses/0020-valid-parentheses.swift
@@ -1,34 +1,16 @@
 class Solution {
     func isValid(_ s: String) -> Bool {
         var stack: [Character] = []
+        let matching: [Character: Character] = [")": "(", "}": "{", "]": "["]
 
         for char in s {
-            if stack.isEmpty && ")]}".contains(char) {
-                return false
-            }
-
-            if "([{".contains(char) {
+            if "({[".contains(char) {
                 stack.append(char)
-            }
-
-            if char == ")" {
-                if stack.last == "(" {
-                    stack.popLast()
-                } else {
+            } else {
+                if stack.isEmpty || stack.last != matching[char] {
                     return false
                 }
-            } else if char == "}" {
-                if stack.last == "{" {
-                    stack.popLast()
-                } else {
-                    return false
-                }
-            } else if char == "]" {
-                if stack.last == "[" {
-                    stack.popLast()
-                } else {
-                    return false
-                }
+                stack.popLast()
             }
         }
 
