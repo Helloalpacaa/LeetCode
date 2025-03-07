bf17b2bf0aa72a1703137e0c56bb461f4860ae81
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-03-07 09:21:39 -0500

    Time: 70 ms (63.64%), Space: 22.7 MB (97.55%) - LeetHub

--- 0528-random-pick-with-weight/0528-random-pick-with-weight.py
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.py
@@ -4,7 +4,6 @@ class Solution:
         # [1, 3]
         # [1, 4]
         # pick a random number from [1, 4], chances of it locates to [1] is 1/4, chances it locates to [2, 3, 4] is 3/4
-        # use binary search to find where the random number should be located
         self.prefix_sum = [0] * len(w)
         self.prefix_sum[0] = w[0]
         for i in range(1, len(w)):
@@ -13,6 +12,7 @@ class Solution:
     def pickIndex(self) -> int:
         # pick a random number from [1, self.prefix_sum[-1]]
         rand = random.randint(1, self.prefix_sum[-1])
+        # use binary search to find where the random number should be located
         index = bisect_left(self.prefix_sum, rand)
         return index
         
