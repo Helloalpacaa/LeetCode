5ac616eaf82a0dc770bb08cb61a93f2d8c2b4e48
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-03-08 07:46:13 -0500

    Time: 23 ms (64.49%), Space: 23.3 MB (79.57%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -6,12 +6,9 @@ class Solution:
         for i in range(1, n):
             ans[i] = ans[i - 1] * nums[i - 1]
         
-        # print(ans)
-        
         right = nums[n - 1]
         for i in range(n - 2, -1, -1):
             ans[i] = ans[i] * right
             right *= nums[i]
-            # print(ans)
         
         return ans
