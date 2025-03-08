a7cface2fb1e47f97b3b681fa3b62f4fcb27a622
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-03-08 09:50:40 -0500

    Time: 519 ms (5%), Space: 37.5 MB (16.07%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.py
@@ -0,0 +1,40 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None or head.next is None:
+            return head
+        
+        prev, slow, fast = None, head, head
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+        
+        prev.next = None
+        
+        l1 = self.sortList(head)
+        l2 = self.sortList(slow)
+
+        return self.merge(l1, l2)
+    
+    def merge(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        curr = dummy
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                curr.next = ListNode(l1.val)
+                l1 = l1.next
+            else:
+                curr.next = ListNode(l2.val)
+                l2 = l2.next
+            curr = curr.next
+        
+        curr.next = l1 if l1 else l2
+        
+        return dummy.next
+    
