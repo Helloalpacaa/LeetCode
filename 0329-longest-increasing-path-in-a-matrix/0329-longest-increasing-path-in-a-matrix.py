49491346f00f55e6b9d6a76bbac7da2f8faabed3
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-03-18 09:08:36 -0400

    Time: 202 ms (27.9%), Space: 26.2 MB (6.12%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -4,27 +4,26 @@ class Solution:
         n = len(matrix[0])
         memo = {}
 
-        def backtracking(i: int, j: int) -> int:
+        def dfs(i: int, j: int) -> int:
             if (i, j) in memo:
                 return memo[(i, j)]
             
-            max_length = 1
+            longest_path = 1
 
-            directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-
+            directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
             for direction in directions:
                 next_i = i + direction[0]
                 next_j = j + direction[1]
 
                 if 0 <= next_i < m and 0 <= next_j < n and matrix[next_i][next_j] > matrix[i][j]:
-                    max_length = max(max_length, backtracking(next_i, next_j) + 1)
+                    longest_path = max(longest_path, 1 + dfs(next_i, next_j))
             
-            memo[(i, j)] = max_length
-            return max_length
+            memo[(i, j)] = longest_path
+            return longest_path
         
-        longest_path = 0
+        ans = 0
         for i in range(m):
             for j in range(n):
-                longest_path = max(longest_path, backtracking(i, j))
+                ans = max(ans, dfs(i, j))
         
-        return longest_path
+        return ans
