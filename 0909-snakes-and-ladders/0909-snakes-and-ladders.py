2bfe7c3dd0d9c11d07e787fdfe25190815f54698
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-03-23 09:16:48 -0400

    Time: 56 ms (9.22%), Space: 18.2 MB (6.6%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.py
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.py
@@ -1,34 +1,41 @@
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
         n = len(board)
-
-        def get_coordinates(pos: int) -> (int, int):
-            pos -= 1
-
-            row = n - 1 - pos // n
-            col = pos % n if (n - 1 - row) % 2 == 0 else n - 1 - pos % n
+        
+        def get_coordinates(points) -> (int, int):
+            points -= 1
+            row = n - 1 - points // n
+            col = points % n if (n - 1 - row) % 2 == 0 else n - 1 - points % n
 
             return (row, col)
         
-        queue = deque([(1, 0)]) # pos, moves
+        queue = deque([1])
         visited = set([1])
+        steps = 0
 
         while queue:
-            pos, moves = queue.popleft()
+            for _ in range(len(queue)):
+                print(queue)
+                points = queue.popleft()
 
-            for i in range(1, 7):
-                next_pos = pos + i
+                for i in range(1, 7):
+                    next_points = points + i
+                    if next_points <= n * n: 
+                        row, col = get_coordinates(next_points)
+                        
+                        if board[row][col] != -1:
+                            next_points = board[row][col]
 
-                row, col = get_coordinates(next_pos)
+                        if next_points == n * n:
+                            return steps + 1
 
-                if board[row][col] != -1:
-                    next_pos = board[row][col]
+                        if next_points not in visited:
+                            queue.append(next_points)
+                            visited.add(next_points)
                 
-                if next_pos == n * n:
-                    return moves + 1
-                
-                if next_pos not in visited:
-                    queue.append((next_pos, moves + 1))
-                    visited.add(next_pos)
-
+            steps += 1
+        
         return -1
+            
+
+
