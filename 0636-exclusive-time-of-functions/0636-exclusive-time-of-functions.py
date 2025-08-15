910796c825830d1da56fa20846437a94b2d75159
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-08-15 00:36:52 -0400

    Time: 7 ms (69.2%), Space: 17.8 MB (69.09%) - LeetHub

--- 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.py
+++ 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.py
@@ -1,7 +1,7 @@
 class Solution:
     def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
         ans = [0] * n
-        stack = []
+        stack = [] #stack表示的是当前处理的function
         prev_time = 0
 
         for log in logs:
@@ -10,10 +10,12 @@ class Solution:
 
             if typ == "start":
                 if stack:
+                    # 开始另一个function，中断前一个function，但是它还会在stack里，等待自己的最终的end
                     ans[stack[-1]] += time - prev_time
                 stack.append(num)
                 prev_time = time
             else:
+                # 碰到end，结束当前function
                 ans[stack.pop()] += time - prev_time + 1
                 prev_time = time + 1
 
