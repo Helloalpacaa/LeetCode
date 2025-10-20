9d0d91363aab91ec6e22cc9ffc63b5a1b2095c7d
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-10-19 21:35:57 -0400

    Time: 2594 ms (5%), Space: 21.4 MB (13.02%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,31 +1,32 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        n = len(nums)
         ans = []
+        n = len(nums)
 
         for i in range(n - 2):
-            if nums[i] > 0:
-                break
-            
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            
-            left = i + 1
-            right = n - 1
-            while left < right:
-                three_sum = nums[i] + nums[left] + nums[right]
-                if three_sum > 0:
-                    right -= 1
-                elif three_sum < 0:
-                    left += 1
-                else:
-                    ans.append([nums[i], nums[left], nums[right]])
-                    left += 1
-                    while left < right and nums[left] == nums[left - 1]:
-                        left += 1
-                    right -= 1
-                    while left < right and nums[right] == nums[right + 1]:
-                        right -= 1
-            
+            for j in range(i + 1, n - 1):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+                two_sum = nums[i] + nums[j]
+                target = 0 - two_sum
+
+                if target < nums[j + 1] or target > nums[n - 1]:
+                    continue
+
+                left = j + 1
+                right = n - 1
+                while left <= right:
+                    mid = (left + right) // 2
+
+                    if nums[mid] == target:
+                        ans.append([nums[i], nums[j], target])
+                        break
+                    elif nums[mid] < target:
+                        left = mid + 1
+                    else:
+                        right = mid - 1
+        
         return ans
