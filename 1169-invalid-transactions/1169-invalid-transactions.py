a1e7f41766127ae4cc02182a8391b84b0560c30e
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-10-20 09:40:57 -0400

    Time: 7 ms (95.84%), Space: 18.6 MB (30.52%) - LeetHub

--- 1169-invalid-transactions/1169-invalid-transactions.py
+++ 1169-invalid-transactions/1169-invalid-transactions.py
@@ -1,25 +1,27 @@
 class Solution:
     def invalidTransactions(self, transactions: List[str]) -> List[str]:
-        parsed = []
-        for t in transactions:
-            name, time, amount, city = t.split(',')
-            parsed.append((name, int(time), int(amount), city))
+        groups: dict[str, list[str]] = defaultdict(list)
+        for i, transaction in enumerate(transactions):
+            name, time, amount, city = transaction.split(",")
+            groups[name].append((int(time), int(amount), city, i, transaction))
+        
+        print(groups)
+        
+        invalid = []
+        for name, group in groups.items():
+            n = len(group)
+            for i in range(n):
+                time_i, amount_i, city_i, index_i, transaction_i = group[i]
 
-        n = len(transactions)
-        invalid = [False] * n
-
-        # Compare all pairs with same name
-        for i in range(n):
-            name_i, time_i, amount_i, city_i = parsed[i]
-            if amount_i > 1000:
-                invalid[i] = True
-
-            for j in range(n):
-                if i == j:
-                    continue
-                name_j, time_j, amount_j, city_j = parsed[j]
-                if name_i == name_j and city_i != city_j and abs(time_i - time_j) <= 60:
-                    invalid[i] = True
-                    invalid[j] = True
-
-        return [transactions[i] for i in range(n) if invalid[i]]
+                if amount_i > 1000:
+                    invalid.append(transaction_i)
+                else:
+                    for j in range(n):
+                        if i == j:
+                            continue
+                        time_j, amount_j, city_j, index_j, transaction_j = group[j]
+                        if abs(time_i - time_j) <= 60 and city_i != city_j and index_i != index_j:
+                            invalid.append(transaction_i)
+                            break
+        
+        return invalid
