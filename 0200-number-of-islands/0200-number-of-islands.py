5ce7d5a16dce62d01fbb8a51aa7eb3d0aebefaab
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-10-19 21:01:54 -0400

    Time: 232 ms (86.62%), Space: 20.1 MB (82.21%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,24 +1,22 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        m, n = len(grid), len(grid[0])
+        directions = [[-1, 0], [0, -1], [1, 0], [0, 1]]
+        
+        def submerge(i: int, j: int) -> None:
+            grid[i][j] = "0"
+
+            for dr in directions:
+                ni, nj = i + dr[0], j + dr[1]
+                if 0 <= ni < m and 0 <= nj < n and grid[ni][nj] == '1':
+                    submerge(ni, nj)
+        
         islands = 0
-        m = len(grid)
-        n = len(grid[0])
-
-        def dfs(i: int, j: int) -> None:
-            if i < 0 or i >= m or j < 0 or j >= n or grid[i][j] == '0':
-                return
-            
-            grid[i][j] = '0'
-
-            dfs(i + 1, j)
-            dfs(i - 1, j)
-            dfs(i, j + 1)
-            dfs(i, j - 1)
-
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == '1':
                     islands += 1
-                    dfs(i, j)
+                    submerge(i, j)
         
         return islands
+
