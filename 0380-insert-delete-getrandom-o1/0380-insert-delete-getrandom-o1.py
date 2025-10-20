b31025e64786807d68bb270fb16250ccabcf0ea8
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-10-19 20:55:58 -0400

    Time: 135 ms (53.41%), Space: 58.1 MB (6.7%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -1,34 +1,43 @@
 class RandomizedSet:
 
     def __init__(self):
-        self.numsArray = [] # array stores all the elements
-        self.numsDict = {} # dict stores all the element: index in array
+        self.arr = [] # store the value
+        self.hashmap = {} # store value: index
         
 
     def insert(self, val: int) -> bool:
-        if val not in self.numsDict:
-            self.numsArray.append(val)
-            self.numsDict[val] = len(self.numsArray) - 1
-            return True
+        if val in self.hashmap:
+            return False
+        
+        self.arr.append(val)
+        index = len(self.arr) - 1
+        self.hashmap[val] = index
 
-        return False
+        return True
         
 
     def remove(self, val: int) -> bool:
-        if val in self.numsDict:
-            index = self.numsDict[val]
-            last_element = self.numsArray[-1]
-            self.numsArray[index] = last_element
-            self.numsArray.pop()
-            self.numsDict[last_element] = index
-            del self.numsDict[val]
-            return True
+        if val not in self.hashmap:
+            return False
         
-        return False
+        index = self.hashmap[val]
+        # print(self.hashmap, val)
+        del self.hashmap[val]
+        # print(self.hashmap, val)
+        last_index = len(self.arr) - 1
+        last_element = self.arr[last_index]
+        if last_index != index:
+            self.arr[index] = last_element
+            self.hashmap[last_element] = index
+        self.arr.pop()
+        # print(self.hashmap, val)
+
+        return True
         
 
     def getRandom(self) -> int:
-        return self.numsArray[random.randint(0, len(self.numsArray) - 1)]
+        return self.arr[random.randint(0, len(self.arr) - 1)]
+
         
 
 
