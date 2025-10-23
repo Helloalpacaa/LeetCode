0fdf6affbaad07fed2da7827af9a0e5aa6637f28
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-10-23 19:26:49 -0400

    Time: 11 ms (71.95%), Space: 17.8 MB (64.24%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,3 +1,11 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s) == Counter(t)
+        s_count = Counter(s)
+        t_count = defaultdict(int)
+
+        for c in t:
+            t_count[c] += 1
+            if t_count[c] > s_count[c]:
+                return False
+        
+        return t_count == s_count
