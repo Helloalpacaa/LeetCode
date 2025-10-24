febb8143d00685ba90e0162009af5e7d5ce70942
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-10-24 19:11:46 -0400

    Time: 105 ms (26.45%), Space: 25.9 MB (94.84%) - LeetHub

--- 1506-find-root-of-n-ary-tree/1506-find-root-of-n-ary-tree.py
+++ 1506-find-root-of-n-ary-tree/1506-find-root-of-n-ary-tree.py
@@ -8,15 +8,28 @@ class Node:
 
 class Solution:
     def findRoot(self, tree: List['Node']) -> 'Node':
-        all_nodes = set()
-        children_nodes = set()
+        # all_nodes = set()
+        # children_nodes = set()
 
-        for node in tree:
-            all_nodes.add(node)
-            for child in node.children:
-                children_nodes.add(child)
+        # for node in tree:
+        #     all_nodes.add(node)
+        #     for child in node.children:
+        #         children_nodes.add(child)
         
-        root = (all_nodes - children_nodes).pop()
+        # root = (all_nodes - children_nodes).pop()
 
-        return root
+        # return root
+
+        xor = 0
+        for node in tree:
+            xor ^= node.val
+            for child in node.children:
+                xor ^= child.val
+        
+        # xor is the root's val, since a ^ a = 0, x ^ 0 = x
+        for node in tree:
+            if node.val == xor:
+                return node
+        
+        return None
 
