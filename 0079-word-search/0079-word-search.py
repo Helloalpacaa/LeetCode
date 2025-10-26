0f01fc90ff8cd0c0aa54a3a45064c1e720913876
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-10-26 19:35:42 -0400

    Time: 4686 ms (34.52%), Space: 18 MB (21.58%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,32 +1,29 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        m = len(board)
-        n = len(board[0])
+        m, n = len(board), len(board[0])
+        directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
         
-        def backtracking(i, j, k) -> bool:
-            if k == len(word):
+        def dfs(i: int, j: int, word_index: int, visited) -> bool:
+            if word_index == len(word) - 1:
                 return True
-
-            if i < 0 or i >= m or j < 0 or j >= n or board[i][j] != word[k]:
-                return False
             
-            tmp = board[i][j]
-            board[i][j] = '#' # change the value is faster then using a set to track the visited grid
-            
-            for direction in [[-1, 0], [1, 0], [0, -1], [0, 1]]:
-                next_i = i + direction[0]
-                next_j = j + direction[1]
-                if backtracking(next_i, next_j, k + 1):
-                    return True
-            
-            board[i][j] = tmp
+            for dr in directions:
+                ni, nj = i + dr[0], j + dr[1]
+                if 0 <= ni < m and 0 <= nj < n and (ni, nj) not in visited and board[ni][nj] == word[word_index + 1]:
+                    visited.add((ni, nj))
+                    if dfs(ni, nj, word_index + 1, visited):
+                        return True
+                    visited.remove((ni, nj))
 
             return False
-
+        
         for i in range(m):
             for j in range(n):
                 if board[i][j] == word[0]:
-                    if backtracking(i, j, 0):
+                    if dfs(i, j, 0, {(i, j)}):
                         return True
         
         return False
+
+            
+            
