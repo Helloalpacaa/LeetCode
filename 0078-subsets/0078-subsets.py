831dd5b85675fe7cb2e88d23560282d451e495b9
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-10-27 20:50:05 -0400

    Time: 0 ms (100%), Space: 17.8 MB (65.2%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,18 +1,17 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        ans = []
-        n = len(nums)
+        res = []
 
-        def backtracking(index: int, path: List[int]) -> None:
-            if index > n:
+        def dfs(index: int, path: list[int]) -> None:
+            res.append(path.copy())
+
+            if index == len(nums):
                 return
-
-            ans.append(path[:])
-
-            for i in range(index, n):
+            
+            for i in range(index, len(nums)):
                 path.append(nums[i])
-                backtracking(i + 1, path)
+                dfs(i + 1, path)
                 path.pop()
         
-        backtracking(0, [])
-        return ans
+        dfs(0, [])
+        return res
