0918cf7dacaa9137bc681e25501566a67ad843c2
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-10-28 11:47:36 -0400

    Time: 2 ms (27.58%), Space: 17.6 MB (87.38%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -1,22 +1,20 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        ans = []
+        res = []
+        used = [False] * 10
 
-        def backtracking(path: List[int], startNum: int, total: int) -> None:
-            if len(path) > k:
+        def dfs(total: int, num: int, path: list[int], used_numbers: int):
+            if total == n and used_numbers == k:
+                res.append(path.copy())
                 return
             
-            if len(path) == k:
-                if total == n:
-                    ans.append(path[:])
-                return
-            
-            for num in range(startNum, 10):
-                path.append(num)
-                total += num
-                backtracking(path, num + 1, total)
-                total -= num
-                path.pop()
+            for i in range(num, 10):
+                if not used[i]:
+                    used[i] = True
+                    path.append(i)
+                    dfs(total + i, i, path, used_numbers + 1)
+                    path.pop()
+                    used[i] = False
         
-        backtracking([], 1, 0)
-        return ans
+        dfs(0, 1, [], 0)
+        return res
