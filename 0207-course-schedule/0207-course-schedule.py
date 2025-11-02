aa3831e1c408cf36de91a0ab8004b30fcfbfe104
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-11-02 08:50:56 -0500

    Time: 7 ms (46.3%), Space: 19.1 MB (61.94%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,29 +1,52 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # DFS
+        # graph = defaultdict(list)
+        # for course, pre in prerequisites:
+        #     graph[pre].append(course)
+        
+        # status = [0] * numCourses # 0: not explored, 1: exploring, 2: explored
+        
+        # def dfs(course) -> bool:
+        #     if status[course] == 1:
+        #         return False
+            
+        #     if status[course] == 2:
+        #         return True
+            
+        #     status[course] = 1
+        #     for nxt in graph[course]:
+        #         if not dfs(nxt):
+        #             return False
+            
+        #     status[course] = 2
+        #     return True
+        
+        # for course, pre in prerequisites:
+        #     if status[pre] == 0:
+        #         if not dfs(pre):
+        #             return False
+        
+        # return True
+
+        # Topological Sort
+        indegree = [0] * numCourses
         graph = defaultdict(list)
+
         for course, pre in prerequisites:
+            indegree[course] += 1
             graph[pre].append(course)
         
-        status = [0] * numCourses # 0: not explored, 1: exploring, 2: explored
+        queue = deque([course for course in range(numCourses) if indegree[course] == 0])
+        total = 0
+        while queue:
+            curr = queue.popleft()
+            total += 1
+
+            for next_course in graph[curr]:
+                indegree[next_course] -= 1
+                if indegree[next_course] == 0:
+                    queue.append(next_course)
         
-        def dfs(course) -> bool:
-            if status[course] == 1:
-                return False
-            
-            if status[course] == 2:
-                return True
-            
-            status[course] = 1
-            for nxt in graph[course]:
-                if not dfs(nxt):
-                    return False
-            
-            status[course] = 2
-            return True
-        
-        for course, pre in prerequisites:
-            if status[pre] == 0:
-                if not dfs(pre):
-                    return False
-        
-        return True
+        return total == numCourses
+
