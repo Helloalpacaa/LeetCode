10883e08ce36d24ff82d10e352201929c46a81c2
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-11-02 17:14:22 -0500

    Time: 22 ms (63.79%), Space: 17.9 MB (58.42%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.py
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.py
@@ -1,37 +1,38 @@
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
         n = len(board)
-
-        def get_coordinates(pos: int) -> (int, int):
-            pos -= 1
-
-            row = n - 1 - pos // n
-            col = pos % n if (n - 1 - row) % 2 == 0 else n - 1 - pos % n
+        
+        def get_coordinates(points) -> (int, int):
+            points -= 1
+            row = n - 1 - points // n
+            col = points % n if (n - 1 - row) % 2 == 0 else n - 1 - points % n
 
             return (row, col)
         
-        queue = deque([(1, 0)]) # pos, moves
+        queue = deque([(1, 0)])
         visited = set([1])
 
         while queue:
-            pos, moves = queue.popleft()
+            points, steps = queue.popleft()
 
             for i in range(1, 7):
-                next_pos = pos + i
-
-                row, col = get_coordinates(next_pos)
+                next_points = points + i
+                row, col = get_coordinates(next_points)
 
                 if board[row][col] != -1:
-                    next_pos = board[row][col]
-                
-                if next_pos == n * n:
-                    return moves + 1
-                
-                if next_pos > n * n:
-                    break
-                
-                if next_pos not in visited:
-                    queue.append((next_pos, moves + 1))
-                    visited.add(next_pos)
+                    next_points = board[row][col]
 
+                if next_points == n * n:
+                    return steps + 1
+                
+                if next_points > n * n:
+                    break
+
+                if next_points not in visited:
+                    queue.append([next_points, steps + 1])
+                    visited.add(next_points)
+        
         return -1
+            
+
+
