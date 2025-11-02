3cb4efc515a029e9cb57d399df80b459c17e64f1
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-11-02 07:09:56 -0500

    Time: 10 ms (15.54%), Space: 19.6 MB (22.47%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,19 +1,32 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        indegree = [0] * numCourses
-        courseList = [[] for _ in range(numCourses)]
+        graph = [[] for _ in range(numCourses)]
         for course, pre in prerequisites:
-            indegree[course] += 1
-            courseList[pre].append(course)
+            graph[course].append(pre)
         
-        queue = deque([c for c in range(numCourses) if indegree[c] == 0])
+        exploring = set()
+        visited = set()
 
-        while queue:
-            pre = queue.popleft()
-            completed += 1
-            for course in courseList[pre]:
-                indegree[course] -= 1
-                if indegree[course] == 0:
-                    queue.append(course)
+        def dfs(course) -> bool:
+            if course in exploring:
+                return False
+            
+            if course in visited:
+                return True
+            
+            exploring.add(course)
+
+            for pre in graph[course]:
+                if not dfs(pre):
+                    return False
+            
+            exploring.remove(course)
+            visited.add(course)
+
+            return True
         
-        return all(indegree[course] == 0 for course in range(numCourses))
+        for course, pre in prerequisites:
+            if not dfs(course):
+                return False
+        
+        return True
