136649a9e90819f34db4037568439d516f134667
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-11-06 09:13:45 -0500

    Time: 96 ms (93.63%), Space: 78.1 MB (43.25%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,62 +1,26 @@
-class Node:
-
-    def __init__(self, key: int, val: int):
-        self.prev = None
-        self.next = None
-        self.key = key
-        self.val = val
-
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cache = {}
+        self.cache = OrderedDict()
         self.capacity = capacity
-        self.head = Node(0, 0)
-        self.tail = Node(0, 0)
-        self.head.next = self.tail
-        self.tail.prev = self.head
         
 
     def get(self, key: int) -> int:
         if key not in self.cache:
             return -1
-        
-        node = self.cache[key]
-        # 把这个node从原本在linkedlist里的位置移到head后面
-        self.remove(node)
-        self.add(node)
 
-        return node.val
+        self.cache.move_to_end(key)
+        return self.cache[key]
         
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            node = self.cache[key]
-            node.val = value
-            self.remove(node)
-            self.add(node)
-        else:
-            node = Node(key, value)
-            self.add(node)
-            self.cache[key] = node
-            if len(self.cache) > self.capacity:
-                LRU = self.tail.prev
-                self.remove(LRU)
-                del self.cache[LRU.key]
-
-
-    def remove(self, node: Node) -> None:
-        next_node = node.next
-        prev = node.prev
-        prev.next = next_node
-        next_node.prev = prev
-    
-    def add(self, node: Node) -> None:
-        next_node = self.head.next
-        self.head.next = node
-        node.prev = self.head
-        node.next = next_node
-        next_node.prev = node
+            self.cache.move_to_end(key)
+            
+        self.cache[key] = value
+        
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last=False)
         
 
 
