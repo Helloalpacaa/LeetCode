e20b389d8430db8872184bca7a85d40c29685e98
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-11-08 22:44:18 -0500

    Time: 3031 ms (88.22%), Space: 17.7 MB (95.73%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,29 +1,29 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         m, n = len(board), len(board[0])
-        directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-        
-        def dfs(i: int, j: int, word_index: int, visited) -> bool:
+        directions = [[-1, 0], [0, -1], [1, 0], [0, 1]]
+
+        def dfs(i: int, j: int, word_index: int) -> bool:
             if word_index == len(word) - 1:
                 return True
             
-            for dr in directions:
-                ni, nj = i + dr[0], j + dr[1]
-                if 0 <= ni < m and 0 <= nj < n and (ni, nj) not in visited and board[ni][nj] == word[word_index + 1]:
-                    visited.add((ni, nj))
-                    if dfs(ni, nj, word_index + 1, visited):
-                        return True
-                    visited.remove((ni, nj))
+            tmp = board[i][j]
+            board[i][j] = '.'
 
+            for di, dj in directions:
+                ni, nj = i + di, j + dj
+                if 0 <= ni < m and 0 <= nj < n and board[ni][nj] == word[word_index + 1]:
+                    if dfs(ni, nj, word_index + 1):
+                        return True
+            
+            board[i][j] = tmp
             return False
-        
+
         for i in range(m):
             for j in range(n):
                 if board[i][j] == word[0]:
-                    if dfs(i, j, 0, {(i, j)}):
+                    if dfs(i, j, 0):
                         return True
         
         return False
 
-            
-            
