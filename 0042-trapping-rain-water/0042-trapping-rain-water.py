fc28be96d95a22f4b128a37385186207af370aea
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-11-08 20:51:59 -0500

    Time: 7 ms (87.42%), Space: 19.3 MB (69.08%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,17 +1,37 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        # monotonic stack
-        # only store elements that smaller than the top of the stack, and pop it when see a larger value
-        stack = []
-        res = 0
-        for i, h in enumerate(height):
-            while stack and height[stack[-1]] < height[i]:
-                base = height[stack.pop()]
-                if stack:
-                    left = stack[-1]
-                    area = (i - left - 1) * (min(height[left], height[i]) - base)
-                    res += area
-            stack.append(i)
+        # stack = []
+        # water = 0
+
+        # for i in range(len(height)):
+        #     while stack and height[i] > height[stack[-1]]:
+        #         base = height[stack.pop()]
+        #         if stack:
+        #             left = stack[-1]
+        #             area = (i - left - 1) * (min(height[left], height[i]) - base)
+        #             water += area
+                
+        #     stack.append(i)
+
+        # return water
+
+        # the highest wall can be seen from left and right
+        left_max, right_max = 0, len(height) - 1
+        water = 0
+        left, right = 0, len(height) - 1
+
+        while left < right:
+            if height[left_max] < height[right_max]:
+                left += 1
+                if height[left] < height[left_max]:
+                    water += height[left_max] - height[left]
+                else:
+                    left_max = left
+            else:
+                right -= 1
+                if height[right] < height[right_max]:
+                    water += height[right_max] - height[right]
+                else:
+                    right_max = right
         
-        return res
-            
+        return water
