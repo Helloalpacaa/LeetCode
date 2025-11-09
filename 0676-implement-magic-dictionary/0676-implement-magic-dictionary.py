7731a80bf2302385ade85fc3fda52cf7f22e4b60
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-11-09 08:47:58 -0500

    Time: 114 ms (46.28%), Space: 20.7 MB (38.28%) - LeetHub

--- 0676-implement-magic-dictionary/0676-implement-magic-dictionary.py
+++ 0676-implement-magic-dictionary/0676-implement-magic-dictionary.py
@@ -1,21 +1,22 @@
 class MagicDictionary:
 
     def __init__(self):
-        self.dict_by_length = defaultdict(list)
-
+        self.pattern = defaultdict(list)
+        
     def buildDict(self, dictionary: List[str]) -> None:
         for word in dictionary:
-            self.dict_by_length[len(word)].append(word)
+            for i in range(len(word)):
+                p = word[:i] + "*" + word[i + 1:]
+                self.pattern[p].append(word)
         
 
     def search(self, searchWord: str) -> bool:
-        if len(searchWord) not in self.dict_by_length:
-            return False
-        
-        for word in self.dict_by_length[len(searchWord)]:
-            diff = sum(searchWord[i] != word[i] for i in range(len(word)))
-            if diff == 1:
-                return True
+        for i in range(len(searchWord)):
+            p = searchWord[:i] + "*" + searchWord[i + 1:]
+            if p in self.pattern:
+                for pa in self.pattern[p]:
+                    if pa != searchWord:
+                        return True
         
         return False
 
