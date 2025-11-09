fdf83896029888679ace36b2d9203a6a5e4309e4
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-11-08 21:30:43 -0500

    Time: 0 ms (100%), Space: 17.7 MB (91.74%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.py
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -2,29 +2,20 @@ class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
         wordset = set(wordDict)
         res = []
-        memo = {}
 
-        def dfs(start: int) -> list[str]:
-            if start == len(s):
-                return [""]
-            if start in memo:
-                return memo[start]
+        def dfs(word_index: str, path: list[str]) -> None:
+            if word_index == len(s):
+                res.append(" ".join(path))
+                return
             
-            sentences = []
-
-            for end in range(start + 1, len(s) + 1):
-                word = s[start: end]
+            for i in range(word_index, len(s)):
+                word = s[word_index: i + 1]
                 if word in wordset:
-                    for sub in dfs(end):
-                        if sub:
-                            # when sub has something
-                            sentences.append(word + " " + sub)
-                        else:
-                            # when sub is ""
-                            sentences.append(word)
-            
-            memo[start] = sentences
-            return sentences
+                    path.append(word)
+                    dfs(i + 1, path)
+                    path.pop()
         
-        return dfs(0)
+        dfs(0, [])
+        return res
+
 
