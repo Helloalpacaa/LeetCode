4f295407710837eedcf9b6259b61ab8915c5422e
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-11-11 19:57:38 -0500

    Time: 203 ms (20.25%), Space: 19.1 MB (99.09%) - LeetHub

---
+++ 0353-design-snake-game/0353-design-snake-game.py
@@ -0,0 +1,37 @@
+class SnakeGame:
+
+    dr = {"R": (0, 1), "L": (0, -1), "U": (-1, 0), "D": (1, 0)}
+
+    def __init__(self, width: int, height: int, food: List[List[int]]):
+        self.food = food
+        self.food_index = 0
+        self.n = width
+        self.m = height
+        self.snake_body = deque([(0, 0)])
+        self.score = 0
+
+
+    def move(self, direction: str) -> int:
+        directions = {"R": (0, 1), "L": (0, -1), "U": (-1, 0), "D": (1, 0)}
+        dr, dc = directions[direction]
+        nr, nc = self.snake_body[0][0] + dr, self.snake_body[0][1] + dc
+
+        if nr < 0 or nr >= self.m or nc < 0 or nc >= self.n or ((nr, nc) in list(self.snake_body)[:len(self.snake_body) - 1]):
+            return -1
+        
+        self.snake_body.appendleft((nr, nc))
+
+        if self.food_index < len(self.food) and [nr, nc] == self.food[self.food_index]:
+            self.score += 1
+            self.food_index += 1
+        else:
+            self.snake_body.pop()
+        
+        return self.score
+        
+        
+
+
+# Your SnakeGame object will be instantiated and called as such:
+# obj = SnakeGame(width, height, food)
+# param_1 = obj.move(direction)
