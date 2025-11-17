8f501ca703092fa45044cefd3164c720e206b121
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-11-17 14:19:12 -0500

    Time: 53 ms (34.38%), Space: 28.9 MB (75.22%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -3,10 +3,12 @@ class Solution:
         n = len(nums)
         # 把[1, ..., n]放在它正确的位置上(n - 1)
         for i in range(n):
-            while 1 <= nums[i] <= n and nums[i] != nums[nums[i] - 1]:
+            # use nums[i] != nums[nums[i] - 1] instead of nums[i] != i + 1 since there might be duplicated elements
+            while 1 <= nums[i] <= n and nums[i] != nums[nums[i] - 1]: 
                 # print(i, nums[i], nums[nums[i] - 1])
                 # nums[i], nums[nums[i] - 1] = nums[nums[i] - 1], nums[i]
                 # print(i, nums)
+                # This seems fine, but the problem is that nums[i] appears multiple times, and nums[i] changes as part of the swap!
                 corrected_index = nums[i] - 1
                 nums[i], nums[corrected_index] = nums[corrected_index], nums[i]
         
