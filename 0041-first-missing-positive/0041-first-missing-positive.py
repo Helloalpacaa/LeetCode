40d9cfc2b8af7bf10c449dd704dd4031cdd459ba
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-11-17 14:19:41 -0500

    Time: 41 ms (63.28%), Space: 28.8 MB (87.89%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -1,18 +1,22 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
+        # starting from 1, check if 1 .. n in the correct place in nums
+        # while the current num is a number that shouldn't be here, switch them
+
+        # the lowest positive numbers starting from [1, 2, 3 ... ]
+        # correct position, if n == 3 -> index should be 2,  index = n - 1
+
+        # 把[1, 2, 3 ... n]放在它正确的位置上
+        # [3, 4, -1, 1]
+        # [-1, 4, 3, 1]
+        # [-1, 1, 3, 4]
+        # [1, -1, 3, 4]
         n = len(nums)
-        # 把[1, ..., n]放在它正确的位置上(n - 1)
         for i in range(n):
-            # use nums[i] != nums[nums[i] - 1] instead of nums[i] != i + 1 since there might be duplicated elements
-            while 1 <= nums[i] <= n and nums[i] != nums[nums[i] - 1]: 
-                # print(i, nums[i], nums[nums[i] - 1])
-                # nums[i], nums[nums[i] - 1] = nums[nums[i] - 1], nums[i]
-                # print(i, nums)
-                # This seems fine, but the problem is that nums[i] appears multiple times, and nums[i] changes as part of the swap!
-                corrected_index = nums[i] - 1
-                nums[i], nums[corrected_index] = nums[corrected_index], nums[i]
-        
-        # 再iterate array，第一个nums[i] != i + 1的就是answer
+            while 1 <= nums[i] <= n and nums[i] != nums[nums[i] - 1]:
+                correct_index = nums[i] - 1
+                nums[i], nums[correct_index] = nums[correct_index], nums[i]
+
         for i in range(n):
             if nums[i] != i + 1:
                 return i + 1
