091a6bd86a166c6be412a249f3ac4a62fe9cce01
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2025-11-22 10:05:43 -0500

    Time: 25 ms (48.7%), Space: 22 MB (34.13%) - LeetHub

--- 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.py
+++ 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.py
@@ -1,7 +1,6 @@
 class Solution:
     def validateBinaryTreeNodes(self, n: int, leftChild: List[int], rightChild: List[int]) -> bool:
         indegree = [0] * n
-        adj = defaultdict(list)
 
         for i in range(n):
             left = leftChild[i]
@@ -11,12 +10,11 @@ class Solution:
                 indegree[left] += 1
                 if indegree[left] > 1:
                     return False
-                adj[i].append(left)
+
             if right != -1:
                 indegree[right] += 1
                 if indegree[right] > 1:
                     return False
-                adj[i].append(right)
         
         root = [indegree[i] for i in range(n) if indegree[i] == 0]
         if len(root) != 1:
@@ -33,8 +31,11 @@ class Solution:
                 return True
 
             state[node] = 1
-            for child in adj[node]:
-                if not dfs(child):
+            if leftChild[node] != -1:
+                if not dfs(leftChild[node]):
+                    return False
+            if rightChild[node] != -1:
+                if not dfs(rightChild[node]):
                     return False
             
             state[node] = 2
@@ -42,7 +43,6 @@ class Solution:
         
         for i in range(n):
             if not dfs(i):
-                print(i)
                 return False
         
         return True
