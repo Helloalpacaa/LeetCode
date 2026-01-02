180258f0f9638fc0ecd0e1743199bb8a4e07aef3
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2026-01-02 15:18:10 -0500

    Time: 23 ms (85.69%), Space: 18 MB (85.14%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.py
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.py
@@ -1,21 +1,24 @@
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
-        min_heap = [(grid[0][0], 0, 0)] # (max_height so far, i , j)
-        directions = [[-1, 0], [0, -1], [0, 1], [1, 0]]
-        visited = set()
-        m, n = len(grid), len(grid[0])
-        
-        while min_heap:
-            t, i, j = heapq.heappop(min_heap)
-            if i == m - 1 and j == n - 1:
-                return t
+        n = len(grid)
+        pq = [(grid[0][0], 0, 0)] # min_heap store (level, i, j)
+        time = 0
 
-            if (i, j) in visited:
-                continue
+        directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+        visited = {(0, 0)}
+
+        while pq:
+            level, i, j = heapq.heappop(pq)
+            time = max(time, level)
+
+            if i == n - 1 and j == n - 1:
+                return time
             
-            visited.add((i, j))
-                  
             for dr in directions:
                 ni, nj = i + dr[0], j + dr[1]
-                if 0 <= ni < m and 0 <= nj < n and (ni, nj) not in visited:
-                    heapq.heappush(min_heap, (max(t, grid[ni][nj]), ni, nj))
+                if 0 <= ni < n and 0 <= nj < n and (ni, nj) not in visited:
+                    visited.add((ni, nj))
+                    heapq.heappush(pq, (grid[ni][nj], ni, nj))
+        
+
+
