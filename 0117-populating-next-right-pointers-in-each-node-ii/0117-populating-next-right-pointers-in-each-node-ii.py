15846743340751b375d074f476bb6e3c1ba550ca
Pan Huijie <67562398+Helloalpacaa@users.noreply.github.com>
2026-01-14 11:30:48 -0500

    Time: 56 ms (6.32%), Space: 20.5 MB (8.28%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
@@ -10,41 +10,24 @@ class Node:
 
 class Solution:
     def connect(self, root: 'Node') -> 'Node':
-        if root is None:
-            return root
+        if not root:
+            return None
         
-        # queue = deque([root])
-
-        # while queue:
-        #     size = len(queue)
-        #     while size > 0:
-        #         node = queue.popleft()
-        #         size -= 1
-        #         if size > 0:
-        #             node.next = queue[0]
+        queue = deque([root])
+        while queue:
+            size = len(queue)
+            while size:
+                node = queue.popleft()
+                size -= 1
                 
-        #         if node.left:
-        #             queue.append(node.left)
-        #         if node.right:
-        #             queue.append(node.right)
+                if size:
+                    node.next = queue[0]
+                else:
+                    node.next = None
+                
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
         
-        # return root
-
-        original_root = root
-
-        while root:
-            dummy = Node()
-            curr = dummy
-
-            while root:
-                if root.left:
-                    curr.next = root.left
-                    curr = curr.next
-                if root.right:
-                    curr.next = root.right
-                    curr = curr.next
-                root = root.next
-            root = dummy.next
-        
-        return original_root
-        
+        return root
